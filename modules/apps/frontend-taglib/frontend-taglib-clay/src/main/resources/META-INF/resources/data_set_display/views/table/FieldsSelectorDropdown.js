f4220668f86afd87eb17b3abb7d978632d6682ca
Ricardo Moura <rikardomoura1@gmail.com>
2021-09-17 17:52:47 -0300

    LPS-139049 Prevent form from being incorrectly submitted in FieldsSelectorDropdown when user hit Enter

--- modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/data_set_display/views/table/FieldsSelectorDropdown.js
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/data_set_display/views/table/FieldsSelectorDropdown.js
@@ -64,6 +64,7 @@ const FieldsSelectorDropdown = ({fields}) => {
 			}
 		>
 			<ClayDropDown.Search
+				formProps={{onSubmit: (event) => event.preventDefault()}}
 				onChange={(event) => setQuery(event.target.value)}
 				value={query}
 			/>
