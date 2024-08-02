7e52235e5baa90ab224cd2020b3919ba76c1e382
Lianne Louie <lianne.louie@liferay.com>
2024-08-02 11:58:34 -0700

    LPD-32703 - add exception mapper

--- modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/jaxrs/exception/mapper/OrganizationNameMustNotExceedMaximumLengthExceptionMapper.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/jaxrs/exception/mapper/OrganizationNameMustNotExceedMaximumLengthExceptionMapper.java
@@ -1,2 +1,39 @@
-package com.liferay.headless.admin.user.internal.jaxrs.exception.mapper;public class OrganizationNameMustNotExceedMaxLengthExceptionMapper {
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.user.internal.jaxrs.exception.mapper;
+
+import com.liferay.portal.kernel.exception.OrganizationNameException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lianne Louie
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Admin.User)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Admin.User.OrganizationNameMustNotExceedMaximumLengthExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class OrganizationNameMustNotExceedMaximumLengthExceptionMapper
+	extends BaseExceptionMapper
+		<OrganizationNameException.MustNotExceedMaximumLength> {
+
+	@Override
+	protected Problem getProblem(
+		OrganizationNameException.MustNotExceedMaximumLength
+			mustNotExceedMaximumLength) {
+
+		return new Problem(mustNotExceedMaximumLength);
+	}
+
 }
