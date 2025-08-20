f8c531498787a1ac2b749f9fed6b2c7cc63d7dc0
Luis Ortiz <luis.ortiz@liferay.com>
2025-08-20 11:58:20 +0200

    LPD-61503 This data cleanup preupgrade process should only handle JournalArticle and JournalFeed directly related data

--- portal-impl/src/com/liferay/portal/upgrade/data/cleanup/JournalDataCleanupPreupgradeProcess.java
+++ portal-impl/src/com/liferay/portal/upgrade/data/cleanup/JournalDataCleanupPreupgradeProcess.java
@@ -5,16 +5,9 @@
 
 package com.liferay.portal.upgrade.data.cleanup;
 
-import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.dao.db.DBInspector;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.upgrade.data.cleanup.DataCleanupPreupgradeProcess;
-import com.liferay.portal.kernel.upgrade.data.cleanup.DefaultAllTablesOrphanReferencesDataCleanupPreupgradeProcess;
 import com.liferay.portal.kernel.upgrade.data.cleanup.FilterableAllTablesOrphanReferencesDataCleanupPreupgradeProcess;
 import com.liferay.portal.kernel.upgrade.data.cleanup.TableOrphanReferencesDataCleanupPreupgradeProcess;
-import com.liferay.portal.kernel.upgrade.data.cleanup.util.OrphanReferencesDataCleanupUtil;
 
 /**
  * @author Luis Ortiz
@@ -24,177 +17,26 @@ public class JournalDataCleanupPreupgradeProcess
 
 	@Override
 	protected void doUpgrade() throws Exception {
-
-		// First delete all JournalArticle related data
-
 		upgrade(
 			new FilterableAllTablesOrphanReferencesDataCleanupPreupgradeProcess(
 				"classNameId = (select classNameId from ClassName_ where " +
 					"value = 'com.liferay.journal.model.JournalArticle')",
 				new String[] {"classNameId"}, "classPK",
 				new String[] {"resourcePrimKey", "id_"}, "JournalArticle"));
-
-		upgrade(
-			new TableOrphanReferencesDataCleanupPreupgradeProcess(
-				null, "resourcePrimKey", "JournalArticleResource",
-				"resourcePrimKey", "JournalArticle"));
-
-		// Upgrade from 7.4
-
-		upgrade(
-			new TableOrphanReferencesDataCleanupPreupgradeProcess(
-				"classNameId = (select classNameId from ClassName_ where " +
-					"value = 'com.liferay.journal.model.JournalArticle') and " +
-						"structureKey != 'BASIC-WEB-CONTENT'",
-				"structureId", "DDMStructure", "DDMStructureId",
-				"JournalArticle"));
-
-		// Upgrade from 7.0 to 7.3
-
-		upgrade(
-			new TableOrphanReferencesDataCleanupPreupgradeProcess(
-				"classNameId = (select classNameId from ClassName_ where " +
-					"value = 'com.liferay.journal.model.JournalArticle') and " +
-						"structureKey != 'BASIC-WEB-CONTENT'",
-				"structureKey", "DDMStructure", "DDMStructureKey",
-				"JournalArticle"));
-
-		// Upgrade from 6.2
-
-		upgrade(
-			new TableOrphanReferencesDataCleanupPreupgradeProcess(
-				"classNameId = (select classNameId from ClassName_ where " +
-					"value = 'com.liferay.journal.model.JournalArticle') and " +
-						"structureKey != 'BASIC-WEB-CONTENT'",
-				"structureKey", "DDMStructure", "structureId",
-				"JournalArticle"));
-
-		// Then delete all JournalFeed related data
-
 		upgrade(
 			new FilterableAllTablesOrphanReferencesDataCleanupPreupgradeProcess(
 				"classNameId = (select classNameId from ClassName_ where " +
 					"value = 'com.liferay.journal.model.JournalFeed')",
 				new String[] {"classNameId"}, "classPK", new String[] {"id_"},
 				"JournalFeed"));
-
-		// Then delete Layout related data
-
-		upgrade(
-			new DataCleanupPreupgradeProcess() {
-
-				@Override
-				protected void doUpgrade() throws Exception {
-					DBInspector dbInspector = new DBInspector(connection);
-					String targetTableName = "Layout";
-
-					if (!dbInspector.hasTable(targetTableName)) {
-						if (_log.isDebugEnabled()) {
-							_log.debug(
-								"Table " + targetTableName + " does not exist");
-						}
-
-						return;
-					}
-
-					String sourceColumnName = "classPK";
-
-					if (!dbInspector.hasColumn(
-							targetTableName, sourceColumnName)) {
-
-						if (_log.isDebugEnabled()) {
-							_log.debug(
-								com.liferay.portal.kernel.util.StringBundler.
-									concat(
-										"Table ", targetTableName,
-										" does not have column ",
-										sourceColumnName));
-						}
-
-						return;
-					}
-
-					String targetColumnName = "plid";
-
-					if (!dbInspector.hasColumn(
-							targetTableName, targetColumnName)) {
-
-						if (_log.isDebugEnabled()) {
-							_log.debug(
-								com.liferay.portal.kernel.util.StringBundler.
-									concat(
-										"Table ", targetTableName,
-										" does not have column ",
-										targetColumnName));
-						}
-
-						return;
-					}
-
-					OrphanReferencesDataCleanupUtil.cleanUpSameTable(
-						StringBundler.concat(
-							"classNameId = (select classNameId from ",
-							"ClassName_ where value = '",
-							Layout.class.getName(), "')"),
-						connection, dbInspector.normalizeName(sourceColumnName),
-						dbInspector.normalizeName(targetTableName),
-						dbInspector.normalizeName(targetColumnName));
-				}
-
-			});
-
-		upgrade(
-			new DefaultAllTablesOrphanReferencesDataCleanupPreupgradeProcess(
-				"plid", "Layout"));
-		upgrade(
-			new FilterableAllTablesOrphanReferencesDataCleanupPreupgradeProcess(
-				StringBundler.concat(
-					"classNameId in (select classNameId from ClassName_ where ",
-					"value = '", Layout.class.getName(), "' or value like '",
-					Layout.class.getName(), "-%')"),
-				new String[] {"classNameId"}, "classPK", new String[] {"plid"},
-				"Layout"));
-
-		// Then SegmentsExperience related data
-
-		upgrade(
-			new DefaultAllTablesOrphanReferencesDataCleanupPreupgradeProcess(
-				"segmentsExperienceId", "SegmentsExperience"));
-
-		// Finally delete all DDMTemplate and DDM Structure related data
-
-		upgrade(
-			new FilterableAllTablesOrphanReferencesDataCleanupPreupgradeProcess(
-				"classNameId = (select classNameId from ClassName_ where " +
-					"value = 'com.liferay.dynamic.data.mapping.model." +
-						"DDMTemplate')",
-				new String[] {"classNameId"}, "classPK",
-				new String[] {"templateId"}, "DDMTemplate"));
-
-		upgrade(
-			new FilterableAllTablesOrphanReferencesDataCleanupPreupgradeProcess(
-				"classNameId = (select classNameId from ClassName_ where " +
-					"value = 'com.liferay.dynamic.data.mapping.model." +
-						"DDMStructure')",
-				new String[] {"classNameId"}, "classPK",
-				new String[] {"structureId"}, "DDMStructure"));
-
 		upgrade(
 			new TableOrphanReferencesDataCleanupPreupgradeProcess(
-				null, "structureId", "DDMStructureVersion", "structureId",
-				"DDMStructure"));
-
+				null, "articlePK", "JournalArticleLocalization", "id_",
+				"JournalArticle"));
 		upgrade(
 			new TableOrphanReferencesDataCleanupPreupgradeProcess(
-				null, "structureVersionId", "DDMField", "structureVersionId",
-				"DDMStructureVersion"));
-		upgrade(
-			new TableOrphanReferencesDataCleanupPreupgradeProcess(
-				null, "structureVersionId", "DDMStructureLayout",
-				"structureVersionId", "DDMStructureVersion"));
+				null, "resourcePrimKey", "JournalArticleResource",
+				"resourcePrimKey", "JournalArticle"));
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		JournalDataCleanupPreupgradeProcess.class);
 
 }
