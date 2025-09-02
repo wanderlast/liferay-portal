b6590bea5a827d411ea8085d41b6a69134739290
Daniel Sanz <daniel.sanz@liferay.com>
2025-09-02 10:45:03 +0200

    LPD-20917 Add useGetStateInURL hook to obtain a curated snapshot of component state from the current url

---
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/utils/useGetStateFromURL.ts
@@ -0,0 +1,55 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {getStateFromURL} from './stateInURL';
+import {IStateInURL} from './types';
+
+type StateInitializer = {
+	[K in keyof IStateInURL]: (value: IStateInURL[K]) => IStateInURL[K] | null;
+};
+
+function useGetStateFromURL({
+	id,
+	stateInitializers,
+}: {
+	id: string;
+	stateInitializers?: StateInitializer;
+}): () => Partial<IStateInURL> {
+	return (): Partial<IStateInURL> => {
+		const state: Partial<IStateInURL> | null = getStateFromURL(id);
+
+		if (!state) {
+			return {};
+		}
+
+		if (!stateInitializers) {
+			return state;
+		}
+
+		const initializedState = {...state};
+
+		for (const key of Object.keys(initializedState) as Array<
+			keyof IStateInURL
+		>) {
+			const stateInitializer = stateInitializers[key];
+			const stateValue = initializedState[key];
+
+			if (stateInitializer && stateValue !== undefined) {
+				const initializedValue = (stateInitializer as any)(stateValue);
+
+				if (initializedValue !== null) {
+					initializedState[key] = initializedValue;
+				}
+				else {
+					delete initializedState[key];
+				}
+			}
+		}
+
+		return initializedState;
+	};
+}
+
+export default useGetStateFromURL;
