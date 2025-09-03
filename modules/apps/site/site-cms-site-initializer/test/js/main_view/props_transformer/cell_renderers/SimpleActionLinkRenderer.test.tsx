b25fed00c2a9fafa6d5ad5ab93b39f528980f470
Olivia Yu <olivia.yu@liferay.com>
2025-09-03 13:20:45 -0700

    LPD-42875 LPD-64011 site-cms-site-initializer: Include test for icon assertion, update props

--- modules/apps/site/site-cms-site-initializer/test/js/main_view/props_transformer/cell_renderers/SimpleActionLinkRenderer.test.tsx
+++ modules/apps/site/site-cms-site-initializer/test/js/main_view/props_transformer/cell_renderers/SimpleActionLinkRenderer.test.tsx
@@ -9,7 +9,7 @@ import React from 'react';
 
 import SimpleActionLinkRenderer from '../../../../../src/main/resources/META-INF/resources/js/main_view/props_transformer/cell_renderers/SimpleActionLinkRenderer';
 
-const testAction = {
+const testActionBase = {
 	data: {id: 'view'},
 	href: 'http://www.test.com',
 };
@@ -19,24 +19,28 @@ const testActionFolder = {
 	href: 'http://www.test.com/folder',
 };
 
-const test = {
-	actions: [testAction, testActionFolder],
-	additionalProps: {
-		fileMimeTypeCssClasses: {
-			default: 'file-icon-color-0',
-		},
-		fileMimeTypeIcons: {
-			default: '',
-		},
-		objectDefinitionCssClasses: {
-			L_BASIC_WEB_CONTENT: 'file-icon-color-1',
-			default: 'file-icon-color-0',
-		},
-		objectDefinitionIcons: {
-			L_BASIC_WEB_CONTENT: 'web-content',
-			default: '',
-		},
+const testAdditionalProps = {
+	fileMimeTypeCssClasses: {
+		default: 'file-icon-color-0',
+		image: 'file-icon-color-3',
 	},
+	fileMimeTypeIcons: {
+		default: 'document-default',
+		image: 'document-image',
+	},
+	objectDefinitionCssClasses: {
+		L_BASIC_WEB_CONTENT: 'content-icon-basic-content',
+		default: 'content-icon-custom-structure',
+	},
+	objectDefinitionIcons: {
+		L_BASIC_WEB_CONTENT: 'web-content',
+		default: 'forms',
+	},
+};
+
+const testBaseProps = {
+	actions: [testActionBase, testActionFolder],
+	additionalProps: testAdditionalProps,
 	itemData: {
 		embedded: {
 			systemProperties: {
@@ -45,99 +49,104 @@ const test = {
 				},
 			},
 		},
-		entryClassName: 'com.liferay.object.model.ObjectEntry',
+		entryClassName: 'com.liferay.object.model.ObjectDefinition#H4T4',
 	},
-	itemDataFolder: {
+	options: {
+		actionId: 'view',
+	},
+	value: 'Web Content Test',
+};
+
+const testFolderProps = {
+	actions: [testActionBase, testActionFolder],
+	additionalProps: testAdditionalProps,
+	itemData: {
 		entryClassName: 'com.liferay.object.model.ObjectEntryFolder',
 	},
 	options: {
 		actionId: 'view',
 	},
-	value: 'Test',
+	value: 'Folder Test',
 };
 
 describe('SimpleActionLinkRenderer. Render the value only.', () => {
 	it('there are no actions', () => {
-		render(
-			<SimpleActionLinkRenderer
-				actions={[]}
-				additionalProps={test.additionalProps}
-				itemData={test.itemData}
-				options={test.options}
-				value={test.value}
-			/>
-		);
+		render(<SimpleActionLinkRenderer {...testBaseProps} actions={[]} />);
 
 		expect(screen.queryByRole('link')).not.toBeInTheDocument();
 
-		expect(screen.getByText(test.value)).toBeInTheDocument();
+		expect(screen.getByText(testBaseProps.value)).toBeInTheDocument();
 	});
 
 	it('empty actionId string', () => {
 		render(
 			<SimpleActionLinkRenderer
-				actions={test.actions}
-				additionalProps={test.additionalProps}
-				itemData={test.itemData}
+				{...testBaseProps}
 				options={{actionId: ''}}
-				value={test.value}
 			/>
 		);
 
 		expect(screen.queryByRole('link')).not.toBeInTheDocument();
 
-		expect(screen.getByText(test.value)).toBeInTheDocument();
+		expect(screen.getByText(testBaseProps.value)).toBeInTheDocument();
 	});
 
 	it('actionId not in available actions', () => {
 		render(
 			<SimpleActionLinkRenderer
-				actions={test.actions}
-				additionalProps={test.additionalProps}
-				itemData={test.itemData}
+				{...testBaseProps}
 				options={{actionId: 'edit'}}
-				value={test.value}
 			/>
 		);
 
 		expect(screen.queryByRole('link')).not.toBeInTheDocument();
 
-		expect(screen.getByText(test.value)).toBeInTheDocument();
+		expect(screen.getByText(testBaseProps.value)).toBeInTheDocument();
 	});
 });
 
 describe('SimpleActionLinkRenderer. Render the link.', () => {
 	it('non-folder action', () => {
-		render(
-			<SimpleActionLinkRenderer
-				actions={test.actions}
-				additionalProps={test.additionalProps}
-				itemData={test.itemData}
-				options={test.options}
-				value={test.value}
-			/>
-		);
+		render(<SimpleActionLinkRenderer {...testBaseProps} />);
 
-		expect(screen.getByRole('link', {name: test.value})).toHaveAttribute(
-			'href',
-			testAction.href
-		);
+		expect(
+			screen.getByRole('link', {name: testBaseProps.value})
+		).toHaveAttribute('href', testActionBase.href);
 	});
 
 	it('folder action', () => {
-		render(
-			<SimpleActionLinkRenderer
-				actions={test.actions}
-				additionalProps={test.additionalProps}
-				itemData={test.itemDataFolder}
-				options={test.options}
-				value={test.value}
-			/>
+		render(<SimpleActionLinkRenderer {...testFolderProps} />);
+
+		expect(
+			screen.getByRole('link', {name: testFolderProps.value})
+		).toHaveAttribute('href', testActionFolder.href);
+	});
+});
+
+describe('SimpleActionLinkRenderer. Show type icon.', () => {
+	it('non-folder item with web content icon', () => {
+		const {container} = render(
+			<SimpleActionLinkRenderer {...testBaseProps} />
 		);
 
-		expect(screen.getByRole('link', {name: test.value})).toHaveAttribute(
-			'href',
-			testActionFolder.href
+		expect(container.querySelectorAll('.sticker')[0]).toHaveClass(
+			'content-icon-basic-content'
+		);
+		expect(screen.getByRole('presentation', {name: ''})).toHaveClass(
+			'lexicon-icon-web-content'
+		);
+	});
+
+	it('folder item with folder icon', () => {
+		const {container} = render(
+			<SimpleActionLinkRenderer {...testFolderProps} />
+		);
+
+		expect(container.querySelectorAll('.sticker')[0]).toHaveClass(
+			'file-icon-color-0'
+		);
+		expect(screen.getByRole('presentation', {name: ''})).toHaveClass(
+			'lexicon-icon-folder'
 		);
 	});
 });
