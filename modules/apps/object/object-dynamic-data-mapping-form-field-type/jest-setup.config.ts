adde99ab5c359bb3dacbc93e4708877eb1607609
Marko Čikoš <marko.cikos@liferay.com>
2025-09-08 15:24:37 +0200

    LPD-47209 Mock CKEditor 5 jest tests, to avoid build issues

--- modules/apps/object/object-dynamic-data-mapping-form-field-type/jest-setup.config.ts
+++ modules/apps/object/object-dynamic-data-mapping-form-field-type/jest-setup.config.ts
@@ -1,14 +1,10 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-/* eslint-env jest */
+// @ts-ignore
 
 jest.mock('ckeditor5', () => ({
 	Plugin: () => {},
 }));
-
-AUI = () => ({
-	use: (key, callback) => callback(key),
-});
--- modules/apps/site/site-cms-site-initializer/jest-setup.config.ts
+++ modules/apps/site/site-cms-site-initializer/jest-setup.config.ts
@@ -3,6 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+// @ts-ignore
+
+jest.mock('ckeditor5', () => ({
+	Plugin: () => {},
+}));
+
 class MockBroadcastChannel {
 	name: string;
 	onmessage: ((event: MessageEvent) => void) | null = null;
