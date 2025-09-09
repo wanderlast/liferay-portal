d33669486982a5b019d246edb57514fc92f525b3
Marko Čikoš <marko.cikos@liferay.com>
2025-09-09 13:24:39 +0200

    LPD-47209 Apply alias consistently

--- modules/_node-scripts/bundle/index.mjs
+++ modules/_node-scripts/bundle/index.mjs
@@ -56,6 +56,7 @@ export default async function main() {
 			globalImports,
 			languageJSON,
 			overridenPackageSymbols,
+			projectAlias,
 			projectDescription,
 			projectEntryPoints,
 			projectWebContextPath
