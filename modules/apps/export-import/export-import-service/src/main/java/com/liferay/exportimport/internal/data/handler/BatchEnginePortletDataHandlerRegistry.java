da73aa32c7fad34c19c1f03c3d5bcf21c440a9ff
Petteri Karttunen <petteri.karttunen@liferay.com>
2025-09-10 21:06:46 +0300

    LPD-64248 export-import-service: Make threadsafe

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/data/handler/BatchEnginePortletDataHandlerRegistry.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/data/handler/BatchEnginePortletDataHandlerRegistry.java
@@ -22,8 +22,8 @@ import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.vulcan.batch.engine.VulcanBatchEngineTaskItemDelegate;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -101,13 +101,14 @@ public class BatchEnginePortletDataHandlerRegistry {
 	@Reference
 	private CompanyLocalService _companyLocalService;
 
-	private ServiceRegistration<FeatureFlagListener> _serviceRegistration;
+	private volatile ServiceRegistration<FeatureFlagListener>
+		_serviceRegistration;
 	private final Map
 		<Long,
 		 ServiceTracker
 			 <VulcanBatchEngineTaskItemDelegate,
 			  ServiceRegistration<PortletDataHandler>>> _serviceTrackers =
-				new HashMap<>();
+				new ConcurrentHashMap<>();
 
 	@Reference
 	private UserLocalService _userLocalService;
