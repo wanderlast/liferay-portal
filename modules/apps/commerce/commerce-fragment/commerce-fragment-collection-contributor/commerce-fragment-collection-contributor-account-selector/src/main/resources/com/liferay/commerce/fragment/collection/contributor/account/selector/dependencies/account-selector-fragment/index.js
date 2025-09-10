d79d5bae7bf0ec7e31e0c745db097cb93bce4241
Alessio Rendina <asilcomando@gmail.com>
2025-09-10 12:47:07 +0200

    LPD-63175 Added commerce account selector fragment collection contributor

---
+++ modules/apps/commerce/commerce-fragment/commerce-fragment-collection-contributor/commerce-fragment-collection-contributor-account-selector/src/main/resources/com/liferay/commerce/fragment/collection/contributor/account/selector/dependencies/account-selector-fragment/index.js
@@ -0,0 +1,70 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+/* eslint-disable no-undef */
+
+const accountSelectorDropdownHeader = fragmentElement.querySelector(
+	`#${fragmentEntryLinkNamespace}-account-selector-dropdown-header`
+);
+const accountSelectorDropdownNextButton = fragmentElement.querySelector(
+	`#${fragmentEntryLinkNamespace}-account-selector-dropdown-next-button`
+);
+const accountSelectorDropdownPrevButton = fragmentElement.querySelector(
+	`#${fragmentEntryLinkNamespace}-account-selector-dropdown-prev-button`
+);
+const panels = Array.from(
+	fragmentElement.querySelectorAll('.account-selector-panel-content')
+).map((value, index) => ({index, value}));
+
+if (layoutMode !== 'edit') {
+	const accountSelectorDropdownMenu = fragmentElement.querySelector(
+		`#${fragmentEntryLinkNamespace}-account-selector-dropdown-menu`
+	);
+
+	accountSelectorDropdownMenu.style.maxHeight = `${configuration.dropdownHeight}px`;
+	accountSelectorDropdownMenu.style.minHeight = `${configuration.dropdownHeight}px`;
+	accountSelectorDropdownMenu.style.maxWidth = `${configuration.dropdownWidth}px`;
+	accountSelectorDropdownMenu.style.minWidth = `${configuration.dropdownWidth}px`;
+
+	accountSelectorDropdownNextButton.addEventListener('click', () => {
+		const step = Number(accountSelectorDropdownHeader.dataset.step);
+
+		handleNav(step, step + 1);
+	});
+
+	accountSelectorDropdownPrevButton.addEventListener('click', () => {
+		const step = Number(accountSelectorDropdownHeader.dataset.step);
+
+		handleNav(step, step - 1);
+	});
+}
+
+function handleNav(step, nextStep) {
+	if (nextStep < 0 || nextStep > panels.length - 1) {
+		return;
+	}
+
+	const panel = panels[nextStep];
+
+	if (panel.index === panels.length - 1) {
+		accountSelectorDropdownNextButton.classList.add('invisible');
+	}
+	else {
+		accountSelectorDropdownNextButton.classList.remove('invisible');
+	}
+
+	if (panel.index === 0) {
+		accountSelectorDropdownPrevButton.classList.add('invisible');
+	}
+	else {
+		accountSelectorDropdownPrevButton.classList.remove('invisible');
+	}
+
+	accountSelectorDropdownHeader.dataset.step = nextStep;
+
+	panels[step].value.classList.add('d-none');
+
+	panel.value.classList.remove('d-none');
+}
