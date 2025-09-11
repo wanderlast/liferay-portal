258f17cbbbb44e41c725fd60a9d3719ac5a31bc5
boton <jorge.gonzalez@liferay.com>
2025-09-11 12:59:19 +0200

    LPD-64188 Simplify test

--- modules/apps/document-library/document-library-web/test/document_library/js/WebdavURLCopyButton.js
+++ modules/apps/document-library/document-library-web/test/document_library/js/WebdavURLCopyButton.js
@@ -1,10 +1,12 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 import '@testing-library/jest-dom/extend-expect';
 import {cleanup, fireEvent, render} from '@testing-library/react';
+import {openToast} from 'frontend-js-components-web';
+
 import React from 'react';
 
 import WebdavURLCopyButton from '../../../src/main/resources/META-INF/resources/js/document_library/WebdavURLCopyButton';
@@ -27,11 +29,8 @@ describe('WebdavURLCopyButton', () => {
 	const url = 'https://example.com/webdav';
 	const id = 'test-input';
 
-	let openToast;
-
 	beforeEach(() => {
 		jest.clearAllMocks();
-		openToast = require('frontend-js-components-web').openToast;
 	});
 
 	afterEach(cleanup);
@@ -39,11 +38,8 @@ describe('WebdavURLCopyButton', () => {
 	it('renders the input with the given URL', () => {
 		const {getByRole} = render(<WebdavURLCopyButton id={id} url={url} />);
 
-		const input = getByRole('textbox');
-		expect(input).toHaveValue(url);
-
-		const button = getByRole('button', {name: /copy-link/i});
-		expect(button).toBeInTheDocument();
+		expect(getByRole('textbox')).toHaveValue(url);
+		expect(getByRole('button', {name: /copy-link/i})).toBeInTheDocument();
 	});
 
 	it('copy the URL to the clipboard and show a success toast', () => {
@@ -51,9 +47,7 @@ describe('WebdavURLCopyButton', () => {
 			<WebdavURLCopyButton id={id} url={url} />
 		);
 
-		const copyButton = getByLabelText('copy-link');
-
-		fireEvent.click(copyButton);
+		fireEvent.click(getByLabelText('copy-link'));
 
 		const successCallback = mockOn.mock.calls.find(
 			([event]) => event === 'success'
