f7c592cb881b22b08688904fdcd4131902d2587d
Brian Chan <brian.chan@liferay.com>
2025-09-11 13:59:21 -0300

    Revert "LPD-65139 refetch order to compensate for potential optimisticlock"
    This reverts commit ebb6827856c47ccfb423ed72a2365c43b75c45a9.

--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderItemLocalServiceImpl.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderItemLocalServiceImpl.java
@@ -257,9 +257,6 @@ public class CommerceOrderItemLocalServiceImpl
 			commerceOrderItemPersistence.update(childCommerceOrderItem);
 		}
 
-		commerceOrder = commerceOrderLocalService.getCommerceOrder(
-			commerceOrderId);
-
 		if (commerceOrder.isManuallyAdjusted() && commerceOrder.isOpen()) {
 			commerceOrder.setManuallyAdjusted(false);
 		}
