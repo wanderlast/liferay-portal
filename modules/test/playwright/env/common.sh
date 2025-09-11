a434daba7ec182343637975e2c9b1d3dc6926956
Brian Chan <brian.chan@liferay.com>
2025-09-11 14:24:29 -0300

    LRCI-5997 SF

--- modules/test/playwright/env/common.sh
+++ modules/test/playwright/env/common.sh
@@ -508,7 +508,7 @@ function prepare_additional_bundles {
 
 		if [[ ${2} == "true" ]]
 		then
-		sed -i "s/lportal/lportal${app_server_bundles_size}/g" "${app_server_dir}/webapps/ROOT/WEB-INF/classes/portal-ext.properties"
+			sed -i "s/lportal/lportal${app_server_bundles_size}/g" "${app_server_dir}/webapps/ROOT/WEB-INF/classes/portal-ext.properties"
 		fi
 	done
 }
