76675af02d9a8fdfab37f97db8c2a36cc82353d5
Dante Wang <dante.wang@liferay.com>
2025-09-12 10:40:33 +0800

    LPD-63794 Inline Const

---
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-http-servlet-impl/src/main/java/com/liferay/portal/osgi/web/http/servlet/internal/constants/Const.java
@@ -0,0 +1,58 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.osgi.web.http.servlet.internal.constants;
+
+/**
+ * @author Dante Wang
+ */
+public class Const {
+
+	public static final String AMP = "&";
+
+	public static final String BLANK = "";
+
+	public static final String CLOSE_PAREN = ")";
+
+	public static final String DOT = ".";
+
+	public static final String[] EMPTY_ARRAY = new String[0];
+
+	public static final String EQUAL = "=";
+
+	public static final String EQUINOX_LEGACY_CONTEXT_HELPER =
+		"equinox.legacy.context.helper";
+
+	public static final String EQUINOX_LEGACY_CONTEXT_SELECT =
+		"equinox.context.select";
+
+	public static final String EQUINOX_LEGACY_HTTP_CONTEXT_INITIATING_ID =
+		"equinox.legacy.http.context.initiating.id";
+
+	public static final String EQUINOX_LEGACY_TCCL_PROP = "equinox.legacy.tccl";
+
+	public static final String FILTER_NAME = "filter-name";
+
+	public static final String FILTER_PRIORITY = "filter-priority";
+
+	public static final String HTTP = "http";
+
+	public static final String LOCALHOST = "localhost";
+
+	public static final String OPEN_PAREN = "(";
+
+	public static final String SERVLET_NAME = "servlet-name";
+
+	public static final String SLASH = "/";
+
+	public static final String SLASH_STAR = "/*";
+
+	public static final String SLASH_STAR_DOT = "/*.";
+
+	public static final String STAR_DOT = "*.";
+
+	public static final String UTF8 = "UTF-8";
+
+}
