68cfe1d60b770d666f4fa13a880bc1a55a2a2157
Pedro Leite <pedro.leite@liferay.com>
2025-09-16 17:28:14 -0300

    LPD-64549 Consolidate the last approved version on object entry table

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/model/listener/ObjectEntryVersionModelListener.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/model/listener/ObjectEntryVersionModelListener.java
@@ -6,8 +6,12 @@
 package com.liferay.object.internal.model.listener;
 
 import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
 import com.liferay.object.model.ObjectEntryVersion;
 import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.object.service.ObjectEntryVersionLocalService;
+import com.liferay.object.util.comparator.ObjectEntryVersionVersionComparator;
 import com.liferay.portal.kernel.audit.AuditMessage;
 import com.liferay.portal.kernel.audit.AuditRouter;
 import com.liferay.portal.kernel.exception.ModelListenerException;
@@ -16,8 +20,13 @@ import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.security.audit.event.generators.constants.EventTypes;
 import com.liferay.portal.security.audit.event.generators.util.AuditMessageBuilder;
+
+import java.io.Serializable;
+
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -30,10 +39,19 @@ public class ObjectEntryVersionModelListener
 	extends BaseModelListener<ObjectEntryVersion> {
 
 	@Override
+	public void onAfterCreate(ObjectEntryVersion objectEntryVersion)
+		throws ModelListenerException {
+
+		_addOrUpdateLatestApprovedObjectEntry(objectEntryVersion);
+	}
+
+	@Override
 	public void onAfterRemove(ObjectEntryVersion objectEntryVersion)
 		throws ModelListenerException {
 
 		try {
+			_addOrUpdateLatestApprovedObjectEntry(objectEntryVersion);
+
 			ObjectDefinition objectDefinition =
 				_objectDefinitionLocalService.getObjectDefinition(
 					objectEntryVersion.getObjectDefinitionId());
@@ -66,6 +84,100 @@ public class ObjectEntryVersionModelListener
 		}
 	}
 
+	@Override
+	public void onAfterUpdate(
+			ObjectEntryVersion originalObjectEntryVersion,
+			ObjectEntryVersion objectEntryVersion)
+		throws ModelListenerException {
+
+		if (objectEntryVersion.getStatus() ==
+				WorkflowConstants.STATUS_IN_TRASH) {
+
+			return;
+		}
+
+		_addOrUpdateLatestApprovedObjectEntry(objectEntryVersion);
+	}
+
+	private void _addOrUpdateLatestApprovedObjectEntry(
+		ObjectEntryVersion objectEntryVersion) {
+
+		try {
+			ObjectEntry objectEntry = _objectEntryLocalService.fetchObjectEntry(
+				objectEntryVersion.getObjectEntryId());
+
+			if ((objectEntry == null) || objectEntry.isApproved()) {
+				_deleteLatestApprovedObjectEntry(
+					objectEntryVersion.getObjectEntryId());
+
+				return;
+			}
+
+			ObjectEntryVersion latestApprovedObjectEntryVersion =
+				_objectEntryVersionLocalService.
+					fetchLatestApprovedObjectEntryVersion(
+						objectEntry.getObjectEntryId(),
+						ObjectEntryVersionVersionComparator.getInstance(false));
+
+			if (latestApprovedObjectEntryVersion == null) {
+				_deleteLatestApprovedObjectEntry(
+					objectEntryVersion.getObjectEntryId());
+
+				return;
+			}
+
+			ObjectEntry latestApprovedObjectEntry =
+				_objectEntryLocalService.fetchObjectEntryByHeadObjectEntryId(
+					objectEntryVersion.getObjectEntryId());
+
+			if (latestApprovedObjectEntry != null) {
+				if (latestApprovedObjectEntry.getVersion() ==
+						latestApprovedObjectEntryVersion.getVersion()) {
+
+					return;
+				}
+
+				_objectEntryLocalService.deleteObjectEntry(
+					latestApprovedObjectEntry);
+			}
+
+			ObjectDefinition objectDefinition =
+				_objectDefinitionLocalService.getObjectDefinition(
+					objectEntryVersion.getObjectDefinitionId());
+
+			com.liferay.object.rest.dto.v1_0.ObjectEntry contentObjectEntry =
+				com.liferay.object.rest.dto.v1_0.ObjectEntry.unsafeToDTO(
+					latestApprovedObjectEntryVersion.getContent());
+
+			Map<String, Object> properties = contentObjectEntry.getProperties();
+
+			_objectEntryLocalService.addLatestApprovedObjectEntry(
+				null, objectEntry.getGroupId(), objectEntry.getUserId(),
+				objectEntry.getObjectEntryId(), objectDefinition,
+				objectEntry.getObjectEntryFolderId(),
+				objectEntry.getDefaultLanguageId(),
+				latestApprovedObjectEntryVersion.getVersion(),
+				(Map<String, Serializable>)properties.get("properties"));
+		}
+		catch (PortalException portalException) {
+			throw new ModelListenerException(portalException);
+		}
+	}
+
+	private void _deleteLatestApprovedObjectEntry(long headObjectEntryId)
+		throws PortalException {
+
+		ObjectEntry latestApprovedObjectEntry =
+			_objectEntryLocalService.fetchObjectEntryByHeadObjectEntryId(
+				headObjectEntryId);
+
+		if (latestApprovedObjectEntry == null) {
+			return;
+		}
+
+		_objectEntryLocalService.deleteObjectEntry(latestApprovedObjectEntry);
+	}
+
 	@Reference
 	private AuditRouter _auditRouter;
 
@@ -74,5 +186,11 @@ public class ObjectEntryVersionModelListener
 
 	@Reference
 	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private ObjectEntryLocalService _objectEntryLocalService;
+
+	@Reference
+	private ObjectEntryVersionLocalService _objectEntryVersionLocalService;
 
 }
