6891062910bd5776a6881767dc2d20d043e54dfb
Wellington Barbosa <welitonpe@gmail.com>
2025-09-17 17:21:25 -0300

    LPD-62458 Created Step Component

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/components/ProductPurchase/Price/index.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/components/ProductPurchase/Price/index.tsx
@@ -20,7 +20,7 @@ const ProductPurchasePrice: React.FC<ProductPurchasePriceProps> = ({
 	<div className="align-items-end d-flex flex-column price-text">
 		<strong className="mr-1">{i18n.translate('price')}</strong>
 
-		<div {...priceProps}>{price}</div>
+		<div {...priceProps}>{price} (Excluding VAT)</div>
 
 		{children}
 	</div>
