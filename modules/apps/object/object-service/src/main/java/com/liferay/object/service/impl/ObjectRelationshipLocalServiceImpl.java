6c1432fa6cea6f98eae08b61b23f2f2159fae21e
Feliphe Marinho <feliphemarinho18@gmail.com>
2025-09-17 18:31:05 -0300

    LPD-64953 refactor: not used

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
@@ -570,33 +570,6 @@ public class ObjectRelationshipLocalServiceImpl
 	}
 
 	@Override
-	public void disableEdge(long objectDefinitionId2) throws PortalException {
-		ObjectDefinition objectDefinition2 =
-			_objectDefinitionPersistence.findByPrimaryKey(objectDefinitionId2);
-
-		for (ObjectRelationship objectRelationship :
-				getObjectRelationshipsByObjectDefinitionId2(
-					objectDefinitionId2)) {
-
-			if (!objectRelationship.isEdge()) {
-				continue;
-			}
-
-			ObjectDefinition objectDefinition1 =
-				_objectDefinitionPersistence.findByPrimaryKey(
-					objectRelationship.getObjectDefinitionId1());
-
-			if (objectDefinition1.getRootObjectDefinitionId() !=
-					objectDefinition2.getRootObjectDefinitionId()) {
-
-				objectRelationship.setEdge(false);
-
-				objectRelationshipPersistence.update(objectRelationship);
-			}
-		}
-	}
-
-	@Override
 	public ObjectRelationship fetchObjectRelationshipByExternalReferenceCode(
 		String externalReferenceCode, long objectDefinitionId1) {
 
