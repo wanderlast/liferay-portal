3b9c5a81b45b8369700be08b988026990dd9df47
Ivica Cardic <ivica.cardic@liferay.com>
2025-09-18 17:22:25 +0200

    LPD-58792 Add suport for icons to recent assets view in CMS

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewHomeRecentAssetsSectionDisplayContext.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewHomeRecentAssetsSectionDisplayContext.java
@@ -6,6 +6,7 @@
 package com.liferay.site.cms.site.initializer.internal.display.context;
 
 import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.document.library.configuration.DLConfiguration;
 import com.liferay.frontend.data.set.model.FDSActionDropdownItem;
 import com.liferay.object.model.ObjectEntryFolder;
 import com.liferay.object.service.ObjectDefinitionService;
@@ -34,7 +35,7 @@ public class ViewHomeRecentAssetsSectionDisplayContext
 
 	public ViewHomeRecentAssetsSectionDisplayContext(
 		DepotEntryLocalService depotEntryLocalService,
-		GroupLocalService groupLocalService,
+		DLConfiguration dlConfiguration, GroupLocalService groupLocalService,
 		HttpServletRequest httpServletRequest, Language language,
 		ObjectDefinitionService objectDefinitionService,
 		ObjectDefinitionSettingLocalService objectDefinitionSettingLocalService,
@@ -43,8 +44,8 @@ public class ViewHomeRecentAssetsSectionDisplayContext
 		Portal portal) {
 
 		super(
-			depotEntryLocalService, null, groupLocalService, httpServletRequest,
-			language, objectDefinitionService,
+			depotEntryLocalService, dlConfiguration, groupLocalService,
+			httpServletRequest, language, objectDefinitionService,
 			objectDefinitionSettingLocalService,
 			objectEntryFolderModelResourcePermission, portal);
 	}
--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/ViewHomeRecentAssetsJSPSectionFragmentRenderer.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/ViewHomeRecentAssetsJSPSectionFragmentRenderer.java
@@ -6,23 +6,32 @@
 package com.liferay.site.cms.site.initializer.internal.fragment.renderer;
 
 import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.document.library.configuration.DLConfiguration;
 import com.liferay.fragment.renderer.FragmentRenderer;
 import com.liferay.object.model.ObjectEntryFolder;
 import com.liferay.object.service.ObjectDefinitionService;
 import com.liferay.object.service.ObjectDefinitionSettingLocalService;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.site.cms.site.initializer.internal.display.context.ViewHomeRecentAssetsSectionDisplayContext;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Christian Dorado
  */
-@Component(service = FragmentRenderer.class)
+@Component(
+	configurationPid = "com.liferay.document.library.configuration.DLConfiguration",
+	service = FragmentRenderer.class
+)
 public class ViewHomeRecentAssetsJSPSectionFragmentRenderer
 	extends BaseJSPSectionFragmentRenderer
 		<ViewHomeRecentAssetsSectionDisplayContext> {
@@ -32,13 +41,20 @@ public class ViewHomeRecentAssetsJSPSectionFragmentRenderer
 		return "home-recent-assets";
 	}
 
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_dlConfiguration = ConfigurableUtil.createConfigurable(
+			DLConfiguration.class, properties);
+	}
+
 	@Override
 	protected ViewHomeRecentAssetsSectionDisplayContext getDisplayContext(
 		HttpServletRequest httpServletRequest) {
 
 		return new ViewHomeRecentAssetsSectionDisplayContext(
-			_depotEntryLocalService, groupLocalService, httpServletRequest,
-			language, _objectDefinitionService,
+			_depotEntryLocalService, _dlConfiguration, groupLocalService,
+			httpServletRequest, language, _objectDefinitionService,
 			_objectDefinitionSettingLocalService,
 			_objectEntryFolderModelResourcePermission, _portal);
 	}
@@ -50,6 +66,8 @@ public class ViewHomeRecentAssetsJSPSectionFragmentRenderer
 
 	@Reference
 	private DepotEntryLocalService _depotEntryLocalService;
+
+	private volatile DLConfiguration _dlConfiguration;
 
 	@Reference
 	private ObjectDefinitionService _objectDefinitionService;
