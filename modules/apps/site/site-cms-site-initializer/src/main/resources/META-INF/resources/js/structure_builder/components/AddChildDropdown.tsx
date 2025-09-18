f9fb65ecd46fc24290362e1699eed2e7463df879
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-09-18 13:19:57 +0200

    LPD-65859 Set dropdown menu height to auto

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/AddChildDropdown.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/AddChildDropdown.tsx
@@ -77,6 +77,7 @@ export default function AddChildDropdown({
 					},
 				]}
 				menuElementAttrs={{className: 'dropdown-menu-cms'}}
+				menuHeight="auto"
 				trigger={
 					triggerType === 'text' ? (
 						<ClayButton displayType="secondary" size="sm">
