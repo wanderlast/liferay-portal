ceeeee99b82848a996a57a0c3b17abb77a8c8c3a
Jürgen Kappler <jurgen.kappler@liferay.com>
2025-09-19 11:51:18 +0200

    LPD-65865 Separate wrapper logic in 2. We need group wrapper since the way it's linked to the depot changes with how we create the depot

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/service/CMSObjectEntryFolderDepotEntryLocalServiceWrapper.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/service/CMSObjectEntryFolderDepotEntryLocalServiceWrapper.java
@@ -7,22 +7,16 @@ package com.liferay.site.cms.site.initializer.internal.service;
 
 import com.liferay.depot.model.DepotEntry;
 import com.liferay.depot.service.DepotEntryLocalServiceWrapper;
-import com.liferay.object.constants.ObjectEntryFolderConstants;
-import com.liferay.object.service.ObjectEntryFolderLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.service.ServiceWrapper;
-import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.site.cms.site.initializer.internal.util.ObjectEntryFolderUtil;
 
 import java.util.Locale;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Jürgen Kappler
@@ -37,7 +31,7 @@ public class CMSObjectEntryFolderDepotEntryLocalServiceWrapper
 
 		DepotEntry depotEntry = super.addDepotEntry(group, serviceContext);
 
-		_addObjectEntryFolders(depotEntry);
+		ObjectEntryFolderUtil.addObjectEntryFolders(depotEntry.getGroupId());
 
 		return depotEntry;
 	}
@@ -51,43 +45,9 @@ public class CMSObjectEntryFolderDepotEntryLocalServiceWrapper
 		DepotEntry depotEntry = super.addDepotEntry(
 			nameMap, descriptionMap, type, serviceContext);
 
-		_addObjectEntryFolders(depotEntry);
+		ObjectEntryFolderUtil.addObjectEntryFolders(depotEntry.getGroupId());
 
 		return depotEntry;
 	}
-
-	private void _addObjectEntryFolders(DepotEntry depotEntry)
-		throws PortalException {
-
-		if (!FeatureFlagManagerUtil.isEnabled(
-				depotEntry.getCompanyId(), "LPD-17564")) {
-
-			return;
-		}
-
-		Group group = depotEntry.getGroup();
-
-		_objectEntryFolderLocalService.addObjectEntryFolder(
-			ObjectEntryFolderConstants.EXTERNAL_REFERENCE_CODE_CONTENTS,
-			group.getGroupId(), group.getCreatorUserId(),
-			ObjectEntryFolderConstants.PARENT_OBJECT_ENTRY_FOLDER_ID_DEFAULT,
-			"",
-			HashMapBuilder.put(
-				LocaleUtil.ENGLISH, "Contents"
-			).build(),
-			"Contents", ServiceContextThreadLocal.getServiceContext());
-		_objectEntryFolderLocalService.addObjectEntryFolder(
-			ObjectEntryFolderConstants.EXTERNAL_REFERENCE_CODE_FILES,
-			group.getGroupId(), group.getCreatorUserId(),
-			ObjectEntryFolderConstants.PARENT_OBJECT_ENTRY_FOLDER_ID_DEFAULT,
-			"",
-			HashMapBuilder.put(
-				LocaleUtil.ENGLISH, "Files"
-			).build(),
-			"Files", ServiceContextThreadLocal.getServiceContext());
-	}
-
-	@Reference
-	private ObjectEntryFolderLocalService _objectEntryFolderLocalService;
 
 }
