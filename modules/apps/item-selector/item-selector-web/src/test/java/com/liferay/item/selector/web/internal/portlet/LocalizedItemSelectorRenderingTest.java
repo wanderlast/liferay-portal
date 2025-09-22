50bbb9949b32efcee74e4ae9188beeac55cfc0e2
Brian Chan <brian.chan@liferay.com>
2025-09-22 16:01:33 -0700

    LPD-62188 auto SF

--- modules/apps/item-selector/item-selector-web/src/test/java/com/liferay/item/selector/web/internal/portlet/LocalizedItemSelectorRenderingTest.java
+++ modules/apps/item-selector/item-selector-web/src/test/java/com/liferay/item/selector/web/internal/portlet/LocalizedItemSelectorRenderingTest.java
@@ -15,7 +15,6 @@ import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import jakarta.portlet.PortletURL;
