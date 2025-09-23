7b4e382476224742ecfcdce8286cfd308604c3c0
Alejandro Tardín <alejandro.tardin@liferay.com>
2025-09-23 08:17:08 +0200

    LPD-63756 Adapting test to the new API structure
    Caused by https://github.com/liferay/liferay-portal/commit/7c31f4a04c8c0a53ffb360f4395395def27e4353
    
    It is an API breaking change, I wonder if that was the right thing to
    do...

--- modules/apps/portal-vulcan/portal-vulcan-test/src/testIntegration/java/com/liferay/portal/vulcan/internal/template/test/RESTClientTemplateContextContributorTest.java
+++ modules/apps/portal-vulcan/portal-vulcan-test/src/testIntegration/java/com/liferay/portal/vulcan/internal/template/test/RESTClientTemplateContextContributorTest.java
@@ -13,6 +13,8 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.HTTPTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.zip.ZipWriter;
 import com.liferay.portal.kernel.zip.ZipWriterFactory;
 import com.liferay.portal.test.rule.Inject;
@@ -63,7 +65,11 @@ public class RESTClientTemplateContextContributorTest {
 		try {
 			JSONObject jsonObject = HTTPTestUtil.invokeToJSONObject(
 				JSONUtil.put(
-					"name", RandomTestUtil.randomString()
+					"name",
+					JSONUtil.put(
+						StringUtil.toLowerCase(
+							String.valueOf(LocaleUtil.getDefault())),
+						RandomTestUtil.randomString())
 				).put(
 					"templateKey",
 					"com.liferay.portal.vulcan.test.site.initializer"
