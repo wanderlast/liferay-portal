a62cf265f9f03578047d27b8be50fcd801c5c20d
Georgel Pop <georgel.pop@liferay.com>
2025-09-23 16:28:25 +0200

    LPD-65783 Fix test ContentPageEditorDefaultEditorConfigurationTest > testRichTextEditor

--- modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/editor/configuration/test/ContentPageEditorDefaultEditorConfigurationTest.java
+++ modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/editor/configuration/test/ContentPageEditorDefaultEditorConfigurationTest.java
@@ -262,7 +262,8 @@ public class ContentPageEditorDefaultEditorConfigurationTest {
 		"b code em h1 h2 h3 h4 h5 h6 hr i p pre strong u [*](*){*}; a[*](*); ",
 		"div[*](*){text-align}; img[*](*){*}; p[*](*); li ol ul ",
 		"[*](*){*};table[border, cellpadding, cellspacing] {width}; tbody td ",
-		"th[scope]; thead tr[scope]; span[*](*){*}; ");
+		"th[scope, colspan, rowspan]; td[colspan, rowspan]; thead tr[scope]; ",
+		"span[*](*){*}; ");
 
 	private static final String _EXTRA_PLUGINS = StringBundler.concat(
 		"autolink,ae_dragresize,ae_addimages,ae_imagealignment,",
