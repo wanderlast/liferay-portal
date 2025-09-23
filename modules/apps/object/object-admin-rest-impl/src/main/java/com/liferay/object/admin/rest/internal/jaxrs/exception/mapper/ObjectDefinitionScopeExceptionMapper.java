a14b4dbf34ea031a0781f3b62fbd64729509aac3
Brian Chan <brian.chan@liferay.com>
2025-09-23 08:49:39 -0700

    LPD-55622 Rename

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/jaxrs/exception/mapper/ObjectDefinitionScopeExceptionMapper.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/jaxrs/exception/mapper/ObjectDefinitionScopeExceptionMapper.java
@@ -20,7 +20,7 @@ import org.osgi.service.component.annotations.Component;
 	property = {
 		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Object.Admin.REST)",
 		"osgi.jaxrs.extension=true",
-		"osgi.jaxrs.name=Liferay.Object.Admin.REST.ObjectScopeExceptionMapper"
+		"osgi.jaxrs.name=Liferay.Object.Admin.REST.ObjectDefinitionScopeExceptionMapper"
 	},
 	service = ExceptionMapper.class
 )
