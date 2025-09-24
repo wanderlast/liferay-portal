27d9d153fa41b5aff9e740d1952ee1dd27e2b67f
Brian Chan <brian.chan@liferay.com>
2025-09-24 08:55:32 -0700

    Revert "LPD-60886 Simplify"
    This reverts commit 4281e73cbdc61e6a1058e5e92f35d416a0b8cb03.

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/layout/display/page/ObjectEntryLayoutDisplayPageObjectProvider.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/layout/display/page/ObjectEntryLayoutDisplayPageObjectProvider.java
@@ -15,9 +15,9 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Locale;
 
@@ -96,17 +96,20 @@ public class ObjectEntryLayoutDisplayPageObjectProvider
 		}
 
 		try {
-			return GetterUtil.getString(
-				_objectEntry.getTitleValue(
-					LocaleUtil.toLanguageId(locale), true));
+			String titleValue = _objectEntry.getTitleValue(
+				LocaleUtil.toLanguageId(locale), true);
+
+			if (Validator.isNotNull(titleValue)) {
+				return titleValue;
+			}
 		}
 		catch (PortalException portalException) {
 			if (_log.isDebugEnabled()) {
 				_log.debug(portalException);
 			}
-
-			return StringPool.BLANK;
 		}
+
+		return StringPool.BLANK;
 	}
 
 	@Override
