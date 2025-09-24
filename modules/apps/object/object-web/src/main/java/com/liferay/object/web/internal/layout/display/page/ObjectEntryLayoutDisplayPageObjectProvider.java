81ff682caa636732568b84ac0b5c1e23f8e256a9
Brian Chan <brian.chan@liferay.com>
2025-09-24 08:06:05 -0700

    LPD-60886 Simplify

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/layout/display/page/ObjectEntryLayoutDisplayPageObjectProvider.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/layout/display/page/ObjectEntryLayoutDisplayPageObjectProvider.java
@@ -15,9 +15,9 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Locale;
 
@@ -96,20 +96,17 @@ public class ObjectEntryLayoutDisplayPageObjectProvider
 		}
 
 		try {
-			String titleValue = _objectEntry.getTitleValue(
-				LocaleUtil.toLanguageId(locale), true);
-
-			if (Validator.isNotNull(titleValue)) {
-				return titleValue;
-			}
+			return GetterUtil.getString(
+				_objectEntry.getTitleValue(
+					LocaleUtil.toLanguageId(locale), true));
 		}
 		catch (PortalException portalException) {
 			if (_log.isDebugEnabled()) {
 				_log.debug(portalException);
 			}
-		}
 
-		return StringPool.BLANK;
+			return StringPool.BLANK;
+		}
 	}
 
 	@Override
