26b0e26a7dbfc9c04ed9cbf48db03241b93b847a
Marko Čikoš <marko.cikos@liferay.com>
2026-03-13 11:58:55 +0100

    LPD-82901 Use deepClone util, to avoid loss of function properties of configuration
    (cherry picked from commit d7ad82ff2ac496ba084b7817e15e4a870d1b1129)

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/utils/client_extensions/loadEditorClientExtensions.ts
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/utils/client_extensions/loadEditorClientExtensions.ts
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import deepClone from '../../../main/util/deepClone';
 import {
 	ClientExtensionResolution,
 	loadClientExtensions,
@@ -27,7 +28,7 @@ export default function loadEditorClientExtensions({
 				})
 			),
 			onLoad: (resolutions: Array<ClientExtensionResolution<any>>) => {
-				let transformedConfig = JSON.parse(JSON.stringify(config));
+				let transformedConfig = deepClone(config);
 
 				resolutions.forEach(({binding: editorTransformer}) => {
 					const editorConfigTransformer =
