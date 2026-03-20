403257decf2a59af952190303d2fd333be98807e
Jose Luis Navarro <joseluis.navarro@liferay.com>
2026-03-20 08:23:50 +0100

    LPD-81551 clear session context after each batch
    (cherry picked from commit 30e03e2dbe7317d1c2598041cf7db41f88cb299e)

--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/BatchEngineExportTaskExecutorImpl.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/BatchEngineExportTaskExecutorImpl.java
@@ -40,6 +40,7 @@ import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.spring.orm.LastSessionRecorderHelperUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MapUtil;
@@ -318,6 +319,8 @@ public class BatchEngineExportTaskExecutorImpl
 							updateBatchEngineExportTask(batchEngineExportTask);
 				}
 
+				clearSessionPersistenceContext();
+
 				if (Thread.interrupted()) {
 					throw new InterruptedException();
 				}
@@ -581,6 +584,10 @@ public class BatchEngineExportTaskExecutorImpl
 			batchEngineExportTask.getCallbackURL(),
 			batchEngineExportTask.getExecuteStatus(),
 			batchEngineExportTask.getBatchEngineExportTaskId());
+	}
+
+	private void clearSessionPersistenceContext() {
+		LastSessionRecorderHelperUtil.syncLastSessionState();
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
