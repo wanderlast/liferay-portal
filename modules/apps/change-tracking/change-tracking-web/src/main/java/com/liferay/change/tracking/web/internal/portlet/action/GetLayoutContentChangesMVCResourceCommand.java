2703cd5c1c6800777560ba733af40c984014ca93
pei-jung <pei-jung.lan@liferay.com>
2026-03-23 22:21:12 -0700

    LPD-82056 Include total

--- modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/portlet/action/GetLayoutContentChangesMVCResourceCommand.java
+++ modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/portlet/action/GetLayoutContentChangesMVCResourceCommand.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR
  * LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
@@ -101,8 +101,14 @@ public class GetLayoutContentChangesMVCResourceCommand
 
 			JSONPortletResponseUtil.writeJSON(
 				resourceRequest, resourceResponse,
-				_getLayoutContentChangesJSONArray(
-					documents, _portal.getHttpServletRequest(resourceRequest)));
+				JSONUtil.put(
+					"layoutContentChanges",
+					_getLayoutContentChangesJSONArray(
+						documents,
+						_portal.getHttpServletRequest(resourceRequest))
+				).put(
+					"total", searchResponse.getTotalHits()
+				));
 		}
 		catch (PortalException portalException) {
 			_log.error(portalException);
