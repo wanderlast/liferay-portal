da0c3a5ceec035b80c747fad7424e0675f5e8d5f
David Truong <david.truong@liferay.com>
2026-03-26 16:50:03 -0500

    LPD-84032 Render layout preview as inline HTML instead of iframe

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/change/tracking/spi/display/LayoutCTDisplayRenderer.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/change/tracking/spi/display/LayoutCTDisplayRenderer.java
@@ -11,25 +11,38 @@ import com.liferay.change.tracking.spi.display.context.DisplayContext;
 import com.liferay.exportimport.kernel.staging.Staging;
 import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.ColorScheme;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.Theme;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.service.permission.LayoutPermission;
+import com.liferay.portal.kernel.servlet.ServletContextPool;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.theme.ThemeUtil;
 import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.HttpComponentsUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.segments.constants.SegmentsWebKeys;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
 import com.liferay.style.book.model.StyleBookEntry;
 import com.liferay.style.book.service.StyleBookEntryLocalService;
 
@@ -39,6 +52,8 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Locale;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -131,6 +146,11 @@ public class LayoutCTDisplayRenderer extends BaseCTDisplayRenderer<Layout> {
 	}
 
 	@Override
+	public boolean isShowPreviewDiff() {
+		return true;
+	}
+
+	@Override
 	public String renderPreview(DisplayContext<Layout> displayContext)
 		throws Exception {
 
@@ -143,42 +163,139 @@ public class LayoutCTDisplayRenderer extends BaseCTDisplayRenderer<Layout> {
 		HttpServletRequest httpServletRequest =
 			displayContext.getHttpServletRequest();
 
-		ThemeDisplay themeDisplay =
-			(ThemeDisplay)httpServletRequest.getAttribute(
-				WebKeys.THEME_DISPLAY);
-
 		Layout previewLayout = layout;
 
 		if (layout.isDenied() || layout.isPending()) {
 			previewLayout = layout.fetchDraftLayout();
 		}
 
-		String languageId = LocaleUtil.toLanguageId(displayContext.getLocale());
+		ThemeDisplay currentThemeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
 
-		String url = HttpComponentsUtil.addParameters(
-			themeDisplay.getPathMain() + "/portal/get_page_preview",
-			"languageId", languageId, "p_l_mode", Constants.PREVIEW,
-			"p_p_state", "undefined", "previewCTCollectionId",
-			layout.getCtCollectionId());
+		ThemeDisplay themeDisplay = (ThemeDisplay)currentThemeDisplay.clone();
 
-		long segmentsExperienceId = ParamUtil.getLong(
-			httpServletRequest, "segmentsExperienceId");
+		themeDisplay.setLayout(previewLayout);
 
-		if (segmentsExperienceId > 0) {
-			url = HttpComponentsUtil.addParameter(
-				url, "segmentsExperienceId", segmentsExperienceId);
+		LayoutSet layoutSet = previewLayout.getLayoutSet();
+
+		themeDisplay.setLocale(displayContext.getLocale());
+
+		themeDisplay.setLayoutSet(layoutSet);
+		themeDisplay.setLookAndFeel(
+			layoutSet.getTheme(), layoutSet.getColorScheme());
+		themeDisplay.setPlid(previewLayout.getPlid());
+		themeDisplay.setScopeGroupId(previewLayout.getGroupId());
+		themeDisplay.setSignedIn(false);
+		themeDisplay.setSiteGroupId(previewLayout.getGroupId());
+
+		User guestUser = _userLocalService.getGuestUser(
+			themeDisplay.getCompanyId());
+
+		themeDisplay.setUser(guestUser);
+
+		long[] currentSegmentsExperienceIds = GetterUtil.getLongValues(
+			httpServletRequest.getAttribute(
+				SegmentsWebKeys.SEGMENTS_EXPERIENCE_IDS));
+		Layout currentLayout = (Layout)httpServletRequest.getAttribute(
+			WebKeys.LAYOUT);
+		Object currentOutputData = httpServletRequest.getAttribute(
+			WebKeys.OUTPUT_DATA);
+		boolean currentPortletDecorate = GetterUtil.getBoolean(
+			httpServletRequest.getAttribute(WebKeys.PORTLET_DECORATE));
+
+		try {
+			long segmentsExperienceId = ParamUtil.getLong(
+				httpServletRequest, "segmentsExperienceId");
+
+			if (segmentsExperienceId <= 0) {
+				segmentsExperienceId =
+					_segmentsExperienceLocalService.
+						fetchDefaultSegmentsExperienceId(
+							previewLayout.getPlid());
+			}
+
+			httpServletRequest.setAttribute(
+				SegmentsWebKeys.SEGMENTS_EXPERIENCE_IDS,
+				new long[] {segmentsExperienceId});
+
+			httpServletRequest.setAttribute(WebKeys.LAYOUT, previewLayout);
+			httpServletRequest.setAttribute(WebKeys.OUTPUT_DATA, null);
+			httpServletRequest.setAttribute(
+				WebKeys.PORTLET_DECORATE, Boolean.FALSE);
+			httpServletRequest.setAttribute(
+				WebKeys.THEME_DISPLAY, themeDisplay);
+
+			ServiceContext serviceContext =
+				ServiceContextThreadLocal.getServiceContext();
+
+			ServiceContext clonedServiceContext =
+				(ServiceContext)serviceContext.clone();
+
+			clonedServiceContext.setPlid(previewLayout.getPlid());
+			clonedServiceContext.setScopeGroupId(previewLayout.getGroupId());
+
+			ServiceContextThreadLocal.pushServiceContext(clonedServiceContext);
+
+			previewLayout.includeLayoutContent(
+				httpServletRequest, displayContext.getHttpServletResponse());
+
+			StringBundler sb = (StringBundler)httpServletRequest.getAttribute(
+				WebKeys.LAYOUT_CONTENT);
+
+			if (sb == null) {
+				return null;
+			}
+
+			String themeHtml = ThemeUtil.include(
+				ServletContextPool.get(_portal.getServletContextName()),
+				httpServletRequest, displayContext.getHttpServletResponse(),
+				"portal_normal.ftl", previewLayout.getTheme(), false);
+
+			httpServletRequest.setAttribute(
+				_STYLES_CONTENT, _extractHeadStyles(themeHtml));
+
+			return _normalizePreview(sb.toString());
 		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to render preview for layout " +
+						previewLayout.getPlid(),
+					exception);
+			}
 
-		url = HttpComponentsUtil.addParameter(
-			url, "selPlid", previewLayout.getPlid());
+			return null;
+		}
+		finally {
+			httpServletRequest.setAttribute(
+				SegmentsWebKeys.SEGMENTS_EXPERIENCE_IDS,
+				currentSegmentsExperienceIds);
+			httpServletRequest.setAttribute(WebKeys.LAYOUT, currentLayout);
+			httpServletRequest.setAttribute(
+				WebKeys.OUTPUT_DATA, currentOutputData);
+			httpServletRequest.setAttribute(
+				WebKeys.PORTLET_DECORATE, currentPortletDecorate);
+			httpServletRequest.setAttribute(
+				WebKeys.THEME_DISPLAY, currentThemeDisplay);
 
-		url = HttpComponentsUtil.addParameter(
-			url, "showUserLocaleOptionsMessage", "false");
+			ServiceContextThreadLocal.popServiceContext();
+		}
+	}
 
-		return StringBundler.concat(
-			"<iframe frameborder=\"0\" onload=\"this.style.height = ",
-			"(this.contentWindow.document.body.scrollHeight+20) + 'px';\" ",
-			"src=\"", url, "\" width=\"100%\"></iframe>");
+	@Override
+	public String renderPreviewStyles(DisplayContext<Layout> displayContext)
+		throws Exception {
+
+		HttpServletRequest httpServletRequest =
+			displayContext.getHttpServletRequest();
+
+		String stylesContent = (String)httpServletRequest.getAttribute(
+			_STYLES_CONTENT);
+
+		httpServletRequest.removeAttribute(_STYLES_CONTENT);
+
+		return stylesContent;
 	}
 
 	@Override
@@ -271,6 +388,61 @@ public class LayoutCTDisplayRenderer extends BaseCTDisplayRenderer<Layout> {
 		);
 	}
 
+	private String _extractHeadStyles(String html) {
+		Matcher headMatcher = _headPattern.matcher(html);
+
+		if (!headMatcher.find()) {
+			return StringPool.BLANK;
+		}
+
+		String headContent = headMatcher.group(1);
+
+		StringBundler sb = new StringBundler();
+
+		Matcher tagMatcher = _styleAndLinkPattern.matcher(headContent);
+
+		while (tagMatcher.find()) {
+			sb.append(tagMatcher.group());
+		}
+
+		return sb.toString();
+	}
+
+	private String _normalizePreview(String html) {
+		String normalized = HtmlUtil.stripBetween(html, "script");
+
+		normalized = _analyticsCollectionPattern.matcher(
+			normalized
+		).replaceAll(
+			StringPool.BLANK
+		);
+
+		normalized = _fragmentIdPattern.matcher(
+			normalized
+		).replaceAll(
+			StringPool.BLANK
+		);
+
+		return normalized;
+	}
+
+	private static final String _STYLES_CONTENT =
+		LayoutCTDisplayRenderer.class + "#STYLES_CONTENT";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutCTDisplayRenderer.class);
+
+	private static final Pattern _analyticsCollectionPattern = Pattern.compile(
+		"data-analytics-targetable-collection=\"[^\"]*\"");
+	private static final Pattern _fragmentIdPattern = Pattern.compile(
+		"id=\"fragment-[a-f0-9-]+\"");
+	private static final Pattern _headPattern = Pattern.compile(
+		"<head[^>]*>([\\s\\S]*?)</head>", Pattern.CASE_INSENSITIVE);
+	private static final Pattern _styleAndLinkPattern = Pattern.compile(
+		"<(?:link[^>]*rel=[\"']stylesheet[\"'][^>]*>|" +
+			"style[^>]*>[\\s\\S]*?</style>)",
+		Pattern.CASE_INSENSITIVE);
+
 	@Reference
 	private LayoutPermission _layoutPermission;
 
@@ -278,9 +450,15 @@ public class LayoutCTDisplayRenderer extends BaseCTDisplayRenderer<Layout> {
 	private Portal _portal;
 
 	@Reference
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
+
+	@Reference
 	private Staging _staging;
 
 	@Reference
 	private StyleBookEntryLocalService _styleBookEntryLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
