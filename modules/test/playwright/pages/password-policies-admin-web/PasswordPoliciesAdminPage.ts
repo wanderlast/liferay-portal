46fa65e2290f9d4564d20ae086768201ba330f0f
Caio Farias <caio.farias@liferay.com>
2026-04-13 17:07:31 -0300

    LPD-85348 Fix fill logic when checkSyntaxToggle isn't checked
    (cherry picked from commit d924fcda78529ff01dca8d5f0940ccbc87696b3d)

--- modules/test/playwright/pages/password-policies-admin-web/PasswordPoliciesAdminPage.ts
+++ modules/test/playwright/pages/password-policies-admin-web/PasswordPoliciesAdminPage.ts
@@ -227,12 +227,15 @@ export class PasswordPoliciesAdminPage {
 			.getByRole('button', {name: 'Password Syntax Checking'})
 			.click();
 		await this.allowDictionaryWordsToggle.check();
-		await this.minLength.fill(String(6));
-		await this.minLowerCase.fill(String(0));
-		await this.minNumbers.fill(String(1));
-		await this.minSymbols.fill(String(0));
-		await this.minUpperCase.fill(String(1));
-		await this.checkSyntaxToggle.uncheck();
+
+		if (await this.checkSyntaxToggle.isChecked()){
+			await this.minLength.fill(String(6));
+			await this.minLowerCase.fill(String(0));
+			await this.minNumbers.fill(String(1));
+			await this.minSymbols.fill(String(0));
+			await this.minUpperCase.fill(String(1));
+			await this.checkSyntaxToggle.uncheck();
+		}
 
 		await this.saveButton.click();
 
