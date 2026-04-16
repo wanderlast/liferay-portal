be14234cb57ff34735cf378ce6729c7ebaadc94e
Juanjo Fernandez <juanjo.fernandez@liferay.com>
2026-04-16 08:32:17 +0200

    LPD-83150 - SF

--- modules/apps/frontend-data-set/frontend-data-set-sample-web/src/main/resources/META-INF/resources/js/AdvancedPropsTransformer.ts
+++ modules/apps/frontend-data-set/frontend-data-set-sample-web/src/main/resources/META-INF/resources/js/AdvancedPropsTransformer.ts
@@ -145,7 +145,7 @@ export default function propsTransformer({
 				...action,
 				isVisible: ({
 					allItemsSelectedActive,
-					selectedItems
+					selectedItems,
 				}: {
 					allItemsSelectedActive: boolean;
 					selectedItems: Array<any>;
