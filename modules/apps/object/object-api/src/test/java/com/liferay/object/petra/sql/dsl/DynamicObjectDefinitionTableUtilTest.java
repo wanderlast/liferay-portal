d627f528d14479fd7a59e6991ae471b37205aa23
Pedro Leite <pedro.leite@liferay.com>
2026-04-21 20:05:12 -0300

    LPD-86826 Update old test

--- modules/apps/object/object-api/src/test/java/com/liferay/object/petra/sql/dsl/DynamicObjectDefinitionTableUtilTest.java
+++ modules/apps/object/object-api/src/test/java/com/liferay/object/petra/sql/dsl/DynamicObjectDefinitionTableUtilTest.java
@@ -6,20 +6,12 @@
 package com.liferay.object.petra.sql.dsl;
 
 import com.liferay.object.constants.ObjectFieldConstants;
-import com.liferay.portal.dao.db.DBManagerImpl;
-import com.liferay.portal.kernel.dao.db.DBManager;
-import com.liferay.portal.kernel.dao.db.DBManagerUtil;
-import com.liferay.portal.kernel.dao.db.DBType;
-import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
-
-import org.mockito.Mockito;
 
 /**
  * @author Gabriel Albuquerque
@@ -31,7 +23,6 @@ public class DynamicObjectDefinitionTableUtilTest {
 	public static final LiferayUnitTestRule liferayUnitTestRule =
 		LiferayUnitTestRule.INSTANCE;
 
-	@Ignore
 	@Test
 	public void testGetDataType() {
 		Assert.assertEquals(
@@ -39,11 +30,6 @@ public class DynamicObjectDefinitionTableUtilTest {
 			DynamicObjectDefinitionTableUtil.getDataType(
 				ObjectFieldConstants.BUSINESS_TYPE_BOOLEAN,
 				ObjectFieldConstants.DB_TYPE_BOOLEAN));
-		Assert.assertEquals(
-			"VARCHAR(5000)",
-			DynamicObjectDefinitionTableUtil.getDataType(
-				ObjectFieldConstants.BUSINESS_TYPE_MULTISELECT_PICKLIST,
-				ObjectFieldConstants.DB_TYPE_STRING));
 		Assert.assertEquals(
 			"VARCHAR(75)",
 			DynamicObjectDefinitionTableUtil.getDataType(
@@ -54,27 +40,6 @@ public class DynamicObjectDefinitionTableUtilTest {
 			DynamicObjectDefinitionTableUtil.getDataType(
 				ObjectFieldConstants.BUSINESS_TYPE_TEXT,
 				ObjectFieldConstants.DB_TYPE_STRING));
-
-		_setDBType(DBType.SQLSERVER);
-
-		Assert.assertEquals(
-			"VARCHAR(4000)",
-			DynamicObjectDefinitionTableUtil.getDataType(
-				ObjectFieldConstants.BUSINESS_TYPE_MULTISELECT_PICKLIST,
-				ObjectFieldConstants.DB_TYPE_STRING));
-	}
-
-	private void _setDBType(DBType dbType) {
-		DBManager dbManager = Mockito.mock(DBManagerImpl.class);
-
-		Mockito.when(
-			dbManager.getDBType()
-		).thenReturn(
-			dbType
-		);
-
-		ReflectionTestUtil.setFieldValue(
-			DBManagerUtil.class, "_dbManager", dbManager);
 	}
 
 }
