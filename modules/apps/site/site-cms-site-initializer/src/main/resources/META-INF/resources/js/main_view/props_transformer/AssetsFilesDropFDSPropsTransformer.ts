6d2352c4bf3690a56f140b0bdd37514f1be55490
Victor Galan <victor.galan@liferay.com>
2026-04-24 17:19:20 +0200

    LPD-86159 Hide option for users without permissions

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/AssetsFilesDropFDSPropsTransformer.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/AssetsFilesDropFDSPropsTransformer.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {IView} from '@liferay/frontend-data-set-web';
+import {IBulkActionItem, IView} from '@liferay/frontend-data-set-web';
 
 import {OBJECT_ENTRY_FOLDER_CLASS_NAME} from '../../common/utils/constants';
 import AssetsFDSPropsTransformer, {
@@ -37,6 +37,28 @@ export default function AssetsFilesDropFDSPropsTransformer({
 
 	return {
 		...assetsData,
+		bulkActions: (assetsData.bulkActions || []).map(
+			(action: IBulkActionItem) => {
+				if (action?.data?.id !== 'find-and-replace') {
+					return action;
+				}
+
+				return {
+					...action,
+					isVisible: ({
+						selectedItems,
+					}: {
+						selectedItems?: Array<any>;
+					} = {}): boolean => {
+						return (
+							selectedItems?.every(
+								(item: any) => item?.actions?.update
+							) ?? false
+						);
+					},
+				};
+			}
+		),
 		fileDropSettings: {
 			enabled: !isCreationMenuEmpty,
 			isDropTarget: ({item}: {item: any}) => {
