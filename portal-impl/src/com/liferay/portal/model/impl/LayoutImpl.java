bdb1489ed6f4390ecf7d2cdac0198b92b8c5bb00
Javier Moral <javier.moral@liferay.com>
2026-04-24 13:22:36 +0200

    LPD-80908 Don't validate parent layout while site template propagation is in progress

--- portal-impl/src/com/liferay/portal/model/impl/LayoutImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/LayoutImpl.java
@@ -6,6 +6,7 @@
 package com.liferay.portal.model.impl;
 
 import com.liferay.document.library.kernel.service.DLAppServiceUtil;
+import com.liferay.exportimport.kernel.staging.MergeLayoutPrototypesThreadLocal;
 import com.liferay.layout.page.template.kernel.provider.util.LayoutPageTemplateEntryLayoutProviderUtil;
 import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.CharPool;
@@ -1258,7 +1259,9 @@ public class LayoutImpl extends LayoutBaseImpl {
 	@Override
 	public boolean isLayoutDeleteable() {
 		try {
-			if (Validator.isNull(getLayoutSetPrototypeLayoutERC())) {
+			if (MergeLayoutPrototypesThreadLocal.isInProgress() ||
+				Validator.isNull(getLayoutSetPrototypeLayoutERC())) {
+
 				return true;
 			}
 
