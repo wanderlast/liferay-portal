c7b98c3a2e0594e159bd9d04686ee83e8d222339
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2026-04-30 14:11:59 -0700

    LPD-88186 add tests

--- modules/apps/segments/segments-service/src/test/java/com/liferay/segments/internal/checker/UserSegmentsEntryMembershipCheckerTest.java
+++ modules/apps/segments/segments-service/src/test/java/com/liferay/segments/internal/checker/UserSegmentsEntryMembershipCheckerTest.java
@@ -198,13 +198,40 @@ public class UserSegmentsEntryMembershipCheckerTest {
 				"(organizationIds in ('2'))", _userAttributes));
 		Assert.assertFalse(
 			UserSegmentsEntryMembershipChecker.isMember(
+				"(organizationIds in ('2', '3', '4'))", _userAttributes));
+		Assert.assertFalse(
+			UserSegmentsEntryMembershipChecker.isMember(
+				"((organizationIds in ('2', '3', '4')) and (classPK eq " +
+					"CLASS_PK))",
+				_userAttributes));
+		Assert.assertFalse(
+			UserSegmentsEntryMembershipChecker.isMember(
 				"not (organizationIds in ('1'))", _userAttributes));
+		Assert.assertFalse(
+			UserSegmentsEntryMembershipChecker.isMember(
+				"not (organizationIds in ('1', '2', '3'))", _userAttributes));
 		Assert.assertTrue(
 			UserSegmentsEntryMembershipChecker.isMember(
 				"(organizationIds in ('1'))", _userAttributes));
 		Assert.assertTrue(
 			UserSegmentsEntryMembershipChecker.isMember(
+				"(organizationIds in ('1', '2', '3'))", _userAttributes));
+		Assert.assertTrue(
+			UserSegmentsEntryMembershipChecker.isMember(
+				"((organizationIds in ('1', '2', '3')) and (classPK eq " +
+					"CLASS_PK))",
+				_userAttributes));
+		Assert.assertTrue(
+			UserSegmentsEntryMembershipChecker.isMember(
+				"((organizationIds in ('2', '3', '4')) or (roleIds in ('1', " +
+					"'5')))",
+				_userAttributes));
+		Assert.assertTrue(
+			UserSegmentsEntryMembershipChecker.isMember(
 				"not (organizationIds in ('2'))", _userAttributes));
+		Assert.assertTrue(
+			UserSegmentsEntryMembershipChecker.isMember(
+				"not (organizationIds in ('2', '3', '4'))", _userAttributes));
 	}
 
 	@Test
