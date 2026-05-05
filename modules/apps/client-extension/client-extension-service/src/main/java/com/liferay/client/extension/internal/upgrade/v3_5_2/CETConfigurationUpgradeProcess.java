455e5bc4c393f4f365b33104e592c408a93d7c82
Drew Brokke <drew.brokke@liferay.com>
2026-05-05 14:11:23 -0500

    LPD-87765 client-extension-service: simplify for readability

--- modules/apps/client-extension/client-extension-service/src/main/java/com/liferay/client/extension/internal/upgrade/v3_5_2/CETConfigurationUpgradeProcess.java
+++ modules/apps/client-extension/client-extension-service/src/main/java/com/liferay/client/extension/internal/upgrade/v3_5_2/CETConfigurationUpgradeProcess.java
@@ -1,18 +1,15 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.client.extension.internal.upgrade.v3_5_2;
 
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-
-import java.sql.PreparedStatement;
 
 /**
  * @author Anthony Chu
+ * @author Drew Brokke
  */
 public class CETConfigurationUpgradeProcess extends UpgradeProcess {
 
@@ -22,22 +19,12 @@ public class CETConfigurationUpgradeProcess extends UpgradeProcess {
 			return;
 		}
 
-		try (PreparedStatement preparedStatement = connection.prepareStatement(
-				"delete from Configuration_ where configurationId like " +
-					"'com.liferay.client.extension.type.configuration." +
-						"CETConfiguration~%'")) {
-
-			int deletedCount = preparedStatement.executeUpdate();
-
-			if (_log.isInfoEnabled()) {
-				_log.info(
-					"Deleted " + deletedCount +
-						" persisted CET configurations");
-			}
-		}
+		runSQL(
+			"delete from Configuration_ where configurationId like '" +
+				_CLASS_NAME_CET_CONFIGURATION + "%'");
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		CETConfigurationUpgradeProcess.class);
+	private static final String _CLASS_NAME_CET_CONFIGURATION =
+		"com.liferay.client.extension.type.configuration.CETConfiguration";
 
 }
