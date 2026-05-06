1105e8d5f9f0a3d93a6d81c30d85eb009e6e071b
Felipe Lorenz <felipe.lorenz@hotmail.com>
2026-05-06 18:35:54 -0300

    LPD-89174 portal-search-tuning-synonyms-web-test: add Object Entry multi-word synonym search test
    https://liferay.atlassian.net/browse/LPD-89174

---
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web-test/src/testIntegration/java/com/liferay/portal/search/tuning/synonyms/web/internal/index/test/ObjectEntrySynonymSearchTest.java
@@ -0,0 +1,285 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.tuning.synonyms.web.internal.index.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.constants.ObjectEntryFolderConstants;
+import com.liferay.object.field.builder.TextObjectFieldBuilder;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.model.bag.ObjectFieldBag;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.object.test.util.ObjectDefinitionTestUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.PortalPreferences;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.service.PortalPreferencesLocalServiceUtil;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
+import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.search.searcher.SearchResponse;
+import com.liferay.portal.search.searcher.Searcher;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.vulcan.util.LocalizedMapUtil;
+
+import jakarta.portlet.ActionRequest;
+
+import java.io.Serializable;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Felipe Lorenz
+ */
+@RunWith(Arquillian.class)
+public class ObjectEntrySynonymSearchTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_companyId = TestPropsValues.getCompanyId();
+		_originalName = PrincipalThreadLocal.getName();
+
+		_user = UserTestUtil.getAdminUser(_companyId);
+
+		PrincipalThreadLocal.setName(_user.getUserId());
+
+		PortalPreferences portalPreferences =
+			PortletPreferencesFactoryUtil.getPortalPreferences(
+				_user.getUserId(), true);
+
+		_originalPortalPreferencesXML = PortletPreferencesFactoryUtil.toXML(
+			portalPreferences);
+
+		portalPreferences.setValue("", "locales", "en_US,pt_BR");
+
+		PortalPreferencesLocalServiceUtil.updatePreferences(
+			_companyId, PortletKeys.PREFS_OWNER_TYPE_COMPANY,
+			PortletPreferencesFactoryUtil.toXML(portalPreferences));
+
+		_addSynonymSets();
+
+		_objectDefinition = _addLocalizedObjectDefinition();
+
+		_addObjectEntry("Query Builder Initiative");
+		_addObjectEntry("UQB Initiative");
+		_addObjectEntry("product delivery Initiative");
+		_addObjectEntry("PD initiative");
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		if (_objectDefinition != null) {
+			_objectDefinitionLocalService.deleteObjectDefinition(
+				_objectDefinition);
+		}
+
+		_deleteSynonymSets();
+
+		PrincipalThreadLocal.setName(_originalName);
+
+		PortalPreferencesLocalServiceUtil.updatePreferences(
+			_companyId, PortletKeys.PREFS_OWNER_TYPE_COMPANY,
+			_originalPortalPreferencesXML);
+	}
+
+	@Test
+	public void testMultiWordSynonymAcronymToFullPhrase() {
+		_assertSearchCount("PD", 2);
+	}
+
+	@Test
+	public void testMultiWordSynonymFullPhraseToAcronym() {
+		_assertSearchCount("product delivery", 2);
+	}
+
+	@Test
+	public void testSingleWordSynonymQueryToUqb() {
+		_assertSearchCount("query", 2);
+	}
+
+	@Test
+	public void testSingleWordSynonymUqbToQuery() {
+		_assertSearchCount("uqb", 2);
+	}
+
+	private static ObjectDefinition _addLocalizedObjectDefinition()
+		throws Exception {
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.addCustomObjectDefinition(
+				null, _user.getUserId(), 0, null, true, false, true, false,
+				true, false, false, false, false, null,
+				LocalizedMapUtil.getLocalizedMap(
+					ObjectDefinitionTestUtil.getRandomName()),
+				ObjectDefinitionTestUtil.getRandomName(), null, null,
+				LocalizedMapUtil.getLocalizedMap(
+					ObjectDefinitionTestUtil.getRandomName()),
+				true, ObjectDefinitionConstants.SCOPE_COMPANY,
+				ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT,
+				Collections.emptyList(),
+				Collections.singletonList(
+					new TextObjectFieldBuilder(
+					).indexed(
+						true
+					).labelMap(
+						LocalizedMapUtil.getLocalizedMap("Title")
+					).localized(
+						true
+					).name(
+						_TITLE_FIELD_NAME
+					).build()),
+				Collections.emptyList(),
+				ServiceContextTestUtil.getServiceContext());
+
+		ObjectFieldBag objectFieldBag = objectDefinition.getObjectFieldBag();
+
+		ObjectField titleField = objectFieldBag.getObjectField(
+			_TITLE_FIELD_NAME);
+
+		_objectDefinitionLocalService.updateTitleObjectFieldId(
+			objectDefinition.getObjectDefinitionId(),
+			titleField.getObjectFieldId());
+
+		return _objectDefinitionLocalService.publishCustomObjectDefinition(
+			_user.getUserId(), objectDefinition.getObjectDefinitionId());
+	}
+
+	private static void _addObjectEntry(String englishValue) throws Exception {
+		_objectEntryLocalService.addObjectEntry(
+			0, _user.getUserId(), _objectDefinition.getObjectDefinitionId(),
+			ObjectEntryFolderConstants.PARENT_OBJECT_ENTRY_FOLDER_ID_DEFAULT,
+			null,
+			HashMapBuilder.<String, Serializable>put(
+				_TITLE_FIELD_NAME + "_i18n",
+				HashMapBuilder.put(
+					LocaleUtil.toLanguageId(LocaleUtil.US), englishValue
+				).build()
+			).build(),
+			ServiceContextTestUtil.getServiceContext());
+	}
+
+	private static void _addSynonymSets() {
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			new MockLiferayPortletActionRequest();
+
+		mockLiferayPortletActionRequest.setAttribute(
+			WebKeys.COMPANY_ID, _companyId);
+
+		ReflectionTestUtil.invoke(
+			_editSynonymSetsMVCActionCommand, "updateSynonymSets",
+			new Class<?>[] {ActionRequest.class, String[].class},
+			mockLiferayPortletActionRequest, _SYNONYM_SETS);
+	}
+
+	private static void _deleteSynonymSets() {
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			new MockLiferayPortletActionRequest();
+
+		mockLiferayPortletActionRequest.addParameter(
+			"deleteAllSynonymSets", "true");
+		mockLiferayPortletActionRequest.setAttribute(
+			WebKeys.COMPANY_ID, _companyId);
+
+		ReflectionTestUtil.invoke(
+			_deleteSynonymSetsMVCActionCommand, "deleteSynonymSets",
+			new Class<?>[] {ActionRequest.class},
+			mockLiferayPortletActionRequest);
+	}
+
+	private void _assertSearchCount(String keyword, int expectedCount) {
+		SearchRequestBuilder searchRequestBuilder =
+			_searchRequestBuilderFactory.builder(
+			).companyId(
+				_companyId
+			).entryClassNames(
+				_objectDefinition.getClassName()
+			).queryString(
+				keyword
+			);
+
+		SearchResponse searchResponse = _searcher.search(
+			searchRequestBuilder.build());
+
+		List<Document> documents = searchResponse.getDocuments71();
+
+		Assert.assertEquals(
+			"Expected " + expectedCount + " documents for keyword \"" +
+				keyword + "\". Request: " + searchResponse.getRequestString() +
+					". Documents: " + documents,
+			expectedCount, documents.size());
+	}
+
+	private static final String[] _SYNONYM_SETS = {
+		"product delivery,PD", "query,uqb"
+	};
+
+	private static final String _TITLE_FIELD_NAME = "able";
+
+	private static Long _companyId;
+
+	@Inject(
+		filter = "mvc.command.name=/synonyms/delete_synonym_sets",
+		type = MVCActionCommand.class
+	)
+	private static MVCActionCommand _deleteSynonymSetsMVCActionCommand;
+
+	@Inject(
+		filter = "mvc.command.name=/synonyms/edit_synonym_sets",
+		type = MVCActionCommand.class
+	)
+	private static MVCActionCommand _editSynonymSetsMVCActionCommand;
+
+	private static ObjectDefinition _objectDefinition;
+
+	@Inject
+	private static ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Inject
+	private static ObjectEntryLocalService _objectEntryLocalService;
+
+	private static String _originalName;
+	private static String _originalPortalPreferencesXML;
+	private static User _user;
+
+	@Inject
+	private Searcher _searcher;
+
+	@Inject
+	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
+
+}
