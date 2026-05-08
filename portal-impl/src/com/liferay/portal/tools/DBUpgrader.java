5d4e5df2e8e7b22fd8775d5f10a8d4fc2416f3e0
Jorge Avalos <jorge.avalos@liferay.com>
2026-05-08 16:15:32 -0700

    LPD-89406 Rotate CompanyInfo.key_ in place during upgrade instead of nulling

--- portal-impl/src/com/liferay/portal/tools/DBUpgrader.java
+++ portal-impl/src/com/liferay/portal/tools/DBUpgrader.java
@@ -16,11 +16,11 @@ import com.liferay.portal.events.StartupHelperUtil;
 import com.liferay.portal.kernel.cache.CacheRegistryUtil;
 import com.liferay.portal.kernel.cache.PortalCacheHelperUtil;
 import com.liferay.portal.kernel.cache.PortalCacheManagerNames;
-import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.db.DBType;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.dependency.manager.DependencyManagerSyncUtil;
+import com.liferay.portal.kernel.encryptor.EncryptorUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.log.Log;
@@ -32,6 +32,7 @@ import com.liferay.portal.kernel.module.util.ServiceLatch;
 import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
+import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceComponentLocalServiceUtil;
 import com.liferay.portal.kernel.service.configuration.ServiceComponentConfiguration;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -59,6 +60,7 @@ import com.liferay.util.dao.orm.CustomSQLUtil;
 import java.io.InputStream;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import java.util.Collection;
@@ -612,9 +614,24 @@ public class DBUpgrader {
 	}
 
 	private static void _updateCompanyKey() throws Exception {
-		DB db = DBManagerUtil.getDB();
+		String sql = "update CompanyInfo set key_ = ? where companyId = ?";
 
-		db.runSQL("update CompanyInfo set key_ = null");
+		CompanyLocalServiceUtil.forEachCompany(
+			company -> {
+				try (Connection connection = DataAccess.getConnection();
+
+					PreparedStatement preparedStatement =
+						connection.prepareStatement(sql)) {
+
+					preparedStatement.setString(
+						1,
+						EncryptorUtil.serializeKey(
+							EncryptorUtil.generateKey()));
+					preparedStatement.setLong(2, company.getCompanyId());
+
+					preparedStatement.executeUpdate();
+				}
+			});
 	}
 
 	private static final String _CLASS_NAME =
