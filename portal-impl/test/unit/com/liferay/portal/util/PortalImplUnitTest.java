b61700f278d9cf9824ec8c71df9fc13889f07769
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-05-08 16:11:23 +0200

    LPD-85590 Move the request-attribute assert inside the try-with-resources

--- portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
@@ -708,14 +708,15 @@ public class PortalImplUnitTest {
 				0, exception, mockHttpServletRequest,
 				new MockHttpServletResponse());
 
+			Assert.assertSame(
+				exception,
+				mockHttpServletRequest.getAttribute(
+					WebKeys.PORTAL_STATUS_EXCEPTION));
+
 			sessionErrorsMockedStatic.verify(
 				() -> SessionErrors.add(
 					mockHttpSession, Exception.class, exception));
 		}
-
-		Assert.assertSame(
-			exception,
-			mockHttpServletRequest.getAttribute(WebKeys.PORTAL_STATUS_EXCEPTION));
 	}
 
 	@Test
