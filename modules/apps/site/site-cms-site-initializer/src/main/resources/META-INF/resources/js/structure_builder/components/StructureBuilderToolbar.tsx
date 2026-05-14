ebacc101e683d284be01ea91ae7e4499be4f480e
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-05-14 16:01:30 +0200

    LPD-83503 Add data-canonical-name Save for mobile

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/StructureBuilderToolbar.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/StructureBuilderToolbar.tsx
@@ -211,7 +211,7 @@ function SaveButton() {
 
 			<ClayButtonWithIcon
 				className="d-md-none"
-				data-canonical-name={Liferay.Language.get('save')}
+				data-canonical-name={Liferay.Language.get('save-mobile')}
 				displayType="secondary"
 				onClick={onSave}
 				size="sm"
