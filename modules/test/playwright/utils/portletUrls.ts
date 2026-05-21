111e21f529b94281a5584fa23dbf2a3d1e097702
Gergely Szalay <gergely.szalay@liferay.com>
2026-05-21 13:01:26 +0200

    LPD-77709 Add Playwright test for translation HTML sanitization

--- modules/test/playwright/utils/portletUrls.ts
+++ modules/test/playwright/utils/portletUrls.ts
@@ -122,6 +122,8 @@ export const PORTLET_URLS = {
 	teams: '/~/control_panel/manage/-/site_teams/teams',
 	templates:
 		'/~/control_panel/manage?p_p_id=com_liferay_template_web_internal_portlet_TemplatePortlet&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view',
+	translation:
+		'/~/control_panel/manage?p_p_id=com_liferay_translation_web_internal_portlet_TranslationPortlet',
 	utilityPages:
 		'/~/control_panel/manage?p_p_id=com_liferay_layout_admin_web_portlet_GroupPagesPortlet&_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_tabs1=utility-pages',
 	widgetPageTemplates:
