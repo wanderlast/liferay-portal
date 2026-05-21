8a2da68fecf26691a2c57d6e4cbe99b9af5311f5
Yuri Monteiro <yuri.monteiro@liferay.com>
2026-05-21 08:46:16 -0300

    LPD-91367 Add regression tests for one-to-many readOnly validation

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/field/util/test/ObjectFieldUtilTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/field/util/test/ObjectFieldUtilTest.java
@@ -8,8 +8,12 @@ package com.liferay.object.field.util.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.dynamic.data.mapping.expression.DDMExpressionFactory;
 import com.liferay.object.constants.ObjectFieldConstants;
+import com.liferay.object.constants.ObjectFieldSettingConstants;
+import com.liferay.object.constants.ObjectRelationshipConstants;
 import com.liferay.object.exception.ObjectFieldReadOnlyException;
+import com.liferay.object.field.builder.ObjectFieldBuilder;
 import com.liferay.object.field.builder.TextObjectFieldBuilder;
+import com.liferay.object.field.setting.builder.ObjectFieldSettingBuilder;
 import com.liferay.object.field.util.ObjectFieldUtil;
 import com.liferay.object.model.ObjectField;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -23,6 +27,7 @@ import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.ClassRule;
@@ -45,6 +50,9 @@ public class ObjectFieldUtilTest {
 
 	@Test
 	public void testValidateReadOnlyObjectFields() throws PortalException {
+
+		// Conditional read only
+
 		ObjectField objectField = new TextObjectFieldBuilder(
 		).labelMap(
 			LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString())
@@ -75,6 +83,110 @@ public class ObjectFieldUtilTest {
 				values
 			).put(
 				objectField.getName(), RandomTestUtil.randomString()
+			).build());
+
+		// One to many relationship
+
+		String objectRelationshipERCObjectFieldName =
+			"a" + RandomTestUtil.randomString();
+
+		ObjectField relationshipObjectField = new ObjectFieldBuilder(
+		).businessType(
+			ObjectFieldConstants.BUSINESS_TYPE_RELATIONSHIP
+		).dbType(
+			ObjectFieldConstants.DB_TYPE_LONG
+		).labelMap(
+			LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString())
+		).name(
+			"a" + RandomTestUtil.randomString()
+		).objectFieldSettings(
+			Collections.singletonList(
+				new ObjectFieldSettingBuilder(
+				).name(
+					ObjectFieldSettingConstants.
+						NAME_OBJECT_RELATIONSHIP_ERC_OBJECT_FIELD_NAME
+				).value(
+					objectRelationshipERCObjectFieldName
+				).build())
+		).readOnly(
+			ObjectFieldConstants.READ_ONLY_TRUE
+		).relationshipType(
+			ObjectRelationshipConstants.TYPE_ONE_TO_MANY
+		).build();
+
+		AssertUtils.assertFailure(
+			ObjectFieldReadOnlyException.class,
+			"Object field " + relationshipObjectField.getName() +
+				" is read only",
+			() -> ObjectFieldUtil.validateReadOnlyObjectFields(
+				_ddmExpressionFactory, new HashMap<>(),
+				Collections.singletonList(relationshipObjectField),
+				HashMapBuilder.<String, Object>put(
+					relationshipObjectField.getName(),
+					RandomTestUtil.randomLong()
+				).build()));
+		AssertUtils.assertFailure(
+			ObjectFieldReadOnlyException.class,
+			"Object field " + relationshipObjectField.getName() +
+				" is read only",
+			() -> ObjectFieldUtil.validateReadOnlyObjectFields(
+				_ddmExpressionFactory, new HashMap<>(),
+				Collections.singletonList(relationshipObjectField),
+				HashMapBuilder.<String, Object>put(
+					objectRelationshipERCObjectFieldName,
+					RandomTestUtil.randomString()
+				).build()));
+
+		String externalReferenceCode = RandomTestUtil.randomString();
+
+		Map<String, Object> existingValues = HashMapBuilder.<String, Object>put(
+			objectRelationshipERCObjectFieldName, externalReferenceCode
+		).put(
+			relationshipObjectField.getName(), RandomTestUtil.randomLong()
+		).build();
+
+		AssertUtils.assertFailure(
+			ObjectFieldReadOnlyException.class,
+			"Object field " + relationshipObjectField.getName() +
+				" is read only",
+			() -> ObjectFieldUtil.validateReadOnlyObjectFields(
+				_ddmExpressionFactory, existingValues,
+				Collections.singletonList(relationshipObjectField),
+				Collections.singletonMap(
+					objectRelationshipERCObjectFieldName, null)));
+		AssertUtils.assertFailure(
+			ObjectFieldReadOnlyException.class,
+			"Object field " + relationshipObjectField.getName() +
+				" is read only",
+			() -> ObjectFieldUtil.validateReadOnlyObjectFields(
+				_ddmExpressionFactory, existingValues,
+				Collections.singletonList(relationshipObjectField),
+				HashMapBuilder.<String, Object>put(
+					objectRelationshipERCObjectFieldName,
+					RandomTestUtil.randomString()
+				).build()));
+
+		ObjectFieldUtil.validateReadOnlyObjectFields(
+			_ddmExpressionFactory, existingValues,
+			Collections.singletonList(relationshipObjectField),
+			HashMapBuilder.<String, Object>put(
+				objectRelationshipERCObjectFieldName, externalReferenceCode
+			).build());
+
+		int objectRelationshipId = RandomTestUtil.randomInt();
+
+		ObjectFieldUtil.validateReadOnlyObjectFields(
+			_ddmExpressionFactory,
+			HashMapBuilder.<String, Object>put(
+				objectRelationshipERCObjectFieldName, externalReferenceCode
+			).put(
+				relationshipObjectField.getName(),
+				Long.valueOf(objectRelationshipId)
+			).build(),
+			Collections.singletonList(relationshipObjectField),
+			HashMapBuilder.<String, Object>put(
+				relationshipObjectField.getName(),
+				Integer.valueOf(objectRelationshipId)
 			).build());
 	}
 
