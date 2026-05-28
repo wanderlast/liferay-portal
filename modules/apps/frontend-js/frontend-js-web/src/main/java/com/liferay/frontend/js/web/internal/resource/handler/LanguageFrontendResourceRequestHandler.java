0cb7f38db2e8fc3e5320d25a0ca69582dcf7c649
joshitagaki <joshua.itagaki@liferay.com>
2026-05-28 11:43:58 -0700

    LPD-92539 Revert "LPD-83084 Provide a compat layer for old JS files cached by browsers. These old JS will still request labels by prepending the languageId in the URL, so it'll appear twice as import map is adding it already. Compat layer ensures LanguageFrontendResourceRequestHandler understands 2 formats:"
    This reverts commit 511660b135760b95e58146fa69533da0acf9eec1.

--- modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/resource/handler/LanguageFrontendResourceRequestHandler.java
+++ modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/resource/handler/LanguageFrontendResourceRequestHandler.java
@@ -79,37 +79,9 @@ public class LanguageFrontendResourceRequestHandler
 
 		String[] requestURIParts = requestURI.split(StringPool.SLASH);
 
-		if ((requestURIParts.length != 3) && (requestURIParts.length != 4)) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"Invalid request " + httpServletRequest.getRequestURI());
-			}
+		if ((requestURIParts.length != 3) ||
+			!Objects.equals(requestURIParts[2], "all.js")) {
 
-			return null;
-		}
-
-		String servletContextPath = requestURIParts[1];
-
-		String allJS = requestURIParts[2];
-
-		// LPD-83084
-
-		if (requestURIParts.length == 4) {
-			servletContextPath = requestURIParts[2];
-			allJS = requestURIParts[3];
-
-			if (!Objects.equals(requestURIParts[0], requestURIParts[1])) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"Invalid request " +
-							httpServletRequest.getRequestURI());
-				}
-
-				return null;
-			}
-		}
-
-		if (!Objects.equals(allJS, "all.js")) {
 			if (_log.isWarnEnabled()) {
 				_log.warn(
 					"Invalid request " + httpServletRequest.getRequestURI());
@@ -121,7 +93,7 @@ public class LanguageFrontendResourceRequestHandler
 		URL url = _hashedFilesRegistry.getResource(
 			StringBundler.concat(
 				portalContextPath, Portal.PATH_MODULE, StringPool.SLASH,
-				servletContextPath, "/language.json"));
+				requestURIParts[1], "/language.json"));
 
 		if (url == null) {
 			if (_log.isWarnEnabled()) {
