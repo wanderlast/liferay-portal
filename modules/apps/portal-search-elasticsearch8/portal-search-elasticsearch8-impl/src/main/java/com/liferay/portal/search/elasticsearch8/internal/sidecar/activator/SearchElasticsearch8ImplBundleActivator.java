1c6e5f571e2c9b8e6807202097b5a9e4908e9a9c
smottal <117469730+smottal@users.noreply.github.com>
2026-05-29 08:32:54 +0200

    LPD-82794 Fix compilation error

--- modules/apps/portal-search-elasticsearch8/portal-search-elasticsearch8-impl/src/main/java/com/liferay/portal/search/elasticsearch8/internal/sidecar/activator/SearchElasticsearch8ImplBundleActivator.java
+++ modules/apps/portal-search-elasticsearch8/portal-search-elasticsearch8-impl/src/main/java/com/liferay/portal/search/elasticsearch8/internal/sidecar/activator/SearchElasticsearch8ImplBundleActivator.java
@@ -14,6 +14,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.module.util.ServiceLatch;
+import com.liferay.portal.kernel.upgrade.util.UpgradeProcessUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.ObjectValuePair;
@@ -65,7 +66,7 @@ public class SearchElasticsearch8ImplBundleActivator
 					sidecarProcessFile));
 		}
 
-		if ((!DBUpgrader.isUpgradeClient() &&
+		if ((!UpgradeProcessUtil.isUpgradeClient() &&
 			 !DBUpgrader.isUpgradeDatabaseAutoRunEnabled() &&
 			 !StartupHelperUtil.isUpgrading()) ||
 			!_hasLegacyElasticsearchConfiguration(bundleContext)) {
