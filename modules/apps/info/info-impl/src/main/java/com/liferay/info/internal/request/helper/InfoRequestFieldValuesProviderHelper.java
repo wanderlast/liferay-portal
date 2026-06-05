366a0b9ea8002809d240ffe95479cf1a593b477f
Shakir Shamim <shakir.shamim@liferay.com>
2026-06-05 11:50:59 +0530

    LPD-93244 Clear unselected checkbox and multiselect fields on submit

--- modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/request/helper/InfoRequestFieldValuesProviderHelper.java
+++ modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/request/helper/InfoRequestFieldValuesProviderHelper.java
@@ -127,11 +127,40 @@ public class InfoRequestFieldValuesProviderHelper {
 			Set<Locale> availableLocales = LanguageUtil.getAvailableLocales(
 				themeDisplay.getSiteGroupId());
 
-			for (String inputName :
-					_getInputNames(
-						infoField, LocaleUtil.toLanguageIds(availableLocales),
-						multipartParameterMap, regularParameterMap)) {
+			Set<String> inputNames = _getInputNames(
+				infoField, LocaleUtil.toLanguageIds(availableLocales),
+				multipartParameterMap, regularParameterMap);
 
+			if (inputNames.isEmpty() && !infoField.isLocalizable()) {
+				if ((infoField.getInfoFieldType() instanceof
+						BooleanInfoFieldType) &&
+					ArrayUtil.contains(
+						checkboxNames, infoField.getUniqueId())) {
+
+					infoFieldValues.put(
+						infoField.getUniqueId(),
+						_getInfoFieldValue(
+							true, infoField, themeDisplay.getLocale(), false));
+
+					continue;
+				}
+
+				if ((infoField.getInfoFieldType() instanceof
+						MultiselectInfoFieldType) &&
+					ArrayUtil.contains(
+						checkboxNames, infoField.getUniqueId())) {
+
+					infoFieldValues.put(
+						infoField.getUniqueId(),
+						_getInfoFieldValue(
+							true, infoField, themeDisplay.getLocale(),
+							Collections.emptyList()));
+
+					continue;
+				}
+			}
+
+			for (String inputName : inputNames) {
 				if (infoField.isLocalizable()) {
 					infoFieldValues.put(
 						inputName,
@@ -170,36 +199,6 @@ public class InfoRequestFieldValuesProviderHelper {
 
 				List<String> regularParameters = regularParameterMap.get(
 					inputName);
-
-				if (regularParameters == null) {
-					if ((infoField.getInfoFieldType() instanceof
-							BooleanInfoFieldType) &&
-						ArrayUtil.contains(
-							checkboxNames, infoField.getUniqueId())) {
-
-						infoFieldValues.put(
-							infoField.getUniqueId(),
-							_getInfoFieldValue(
-								true, infoField, themeDisplay.getLocale(),
-								false));
-
-						continue;
-					}
-
-					if ((infoField.getInfoFieldType() instanceof
-							MultiselectInfoFieldType) &&
-						ArrayUtil.contains(
-							checkboxNames, infoField.getUniqueId())) {
-
-						infoFieldValues.put(
-							infoField.getUniqueId(),
-							_getInfoFieldValue(
-								true, infoField, themeDisplay.getLocale(),
-								Collections.emptyList()));
-
-						continue;
-					}
-				}
 
 				Object value = _parseValue(
 					groupId, infoField, themeDisplay.getLocale(),
