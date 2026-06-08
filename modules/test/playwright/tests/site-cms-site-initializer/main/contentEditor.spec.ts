ee477a79ddfb4c4d5e36b300bcd2710a5c5e98a7
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-06-08 17:41:40 +0200

    LPD-93785 Add test

--- modules/test/playwright/tests/site-cms-site-initializer/main/contentEditor.spec.ts
+++ modules/test/playwright/tests/site-cms-site-initializer/main/contentEditor.spec.ts
@@ -1378,7 +1378,7 @@ test.describe('Schedule Publication', () => {
 
 test(
 	'The Rich Text required error only occurs when the field has no value',
-	{tag: '@LPD-69695'},
+	{tag: ['@LPD-69695', '@LPD-93785']},
 	async ({contentsPage, page, structureBuilderPage}) => {
 
 		// Create a structure with a required Rich Text field
@@ -1415,6 +1415,10 @@ test(
 		await expect(
 			page.locator('.rich-text-input [data-required-error]')
 		).toHaveText('This field is required.');
+
+		// The AI Creator button is not available in the CMS Rich Text editor
+
+		await expect(page.getByTitle('Create AI Content')).toBeHidden();
 
 		// Fill the Rich Text field and publish the content
 
