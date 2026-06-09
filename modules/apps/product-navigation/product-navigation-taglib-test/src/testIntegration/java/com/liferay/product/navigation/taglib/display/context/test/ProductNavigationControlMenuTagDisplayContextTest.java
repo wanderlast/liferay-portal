982123a78fdf29ab219952deca9c3214cd83272a
Gabriel Prates <gabsprates@gmail.com>
2026-06-09 15:10:25 +0200

    LPD-93507 Move control menu tag test to product-navigation-taglib-test

--- modules/apps/product-navigation/product-navigation-taglib-test/src/testIntegration/java/com/liferay/product/navigation/taglib/display/context/test/ProductNavigationControlMenuTagDisplayContextTest.java
+++ modules/apps/product-navigation/product-navigation-taglib-test/src/testIntegration/java/com/liferay/product/navigation/taglib/display/context/test/ProductNavigationControlMenuTagDisplayContextTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.product.navigation.control.menu.display.context.test;
+package com.liferay.product.navigation.taglib.display.context.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.layout.test.util.LayoutTestUtil;
