d98f06d988361f77cad6e37231b7b8b1ddac67ef
Carlos Correa <carlos.correa@liferay.com>
2026-06-11 10:32:19 +0200

    LPD-93823 Adapt test, deleted Site Template pages are no longer removed from linked Sites

--- modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutSetPrototypePropagationTest.java
+++ modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutSetPrototypePropagationTest.java
@@ -1082,8 +1082,14 @@ public class LayoutSetPrototypePropagationTest
 
 		propagateChanges(group);
 
-		Assert.assertEquals(
-			_initialPrototypeLayoutsCount, getGroupLayoutCount());
+		if (linkEnabled) {
+			Assert.assertEquals(
+				_initialPrototypeLayoutsCount + 2, getGroupLayoutCount());
+		}
+		else {
+			Assert.assertEquals(
+				_initialPrototypeLayoutsCount, getGroupLayoutCount());
+		}
 	}
 
 	protected void doTestLayoutPropagationWithLayoutPrototype(
