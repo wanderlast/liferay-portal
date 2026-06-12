419c3a1dd3e3b177019ef7f06e900ca7d24a4780
Carlos Correa <carlos.correa@liferay.com>
2026-06-12 11:48:11 +0200

    LPD-87027 Restrict layout autopropagation on read to page template links
    Previously every layout was lazily merged from its prototype when read.
    Stop doing that for site template links: pages propagated from a site
    template no longer merge on read, since their source pages within the
    site template are merged by the site template sync background task
    before exporting the LAR. Only fire the lazy merge for pages that have a
    direct page template link and no site template link.

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -1863,16 +1863,6 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 			LayoutSet layoutSet = _layoutSetLocalService.getLayoutSet(
 				groupId, privateLayout);
 
-			if (layoutSet.isLayoutSetPrototypeLinkActive() &&
-				!_mergeLayouts(
-					group, layoutSet, groupId, privateLayout, parentLayoutId,
-					start, end, orderByComparator)) {
-
-				return layoutPersistence.findByG_P_P(
-					groupId, privateLayout, parentLayoutId, start, end,
-					orderByComparator);
-			}
-
 			List<Layout> layouts = layoutPersistence.findByG_P_P(
 				groupId, privateLayout, parentLayoutId, start, end,
 				orderByComparator);
@@ -4319,16 +4309,6 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 			Group group = _groupPersistence.findByPrimaryKey(
 				layoutSet.getGroupId());
 
-			if (layoutSet.isLayoutSetPrototypeLinkActive() &&
-				!_mergeLayouts(
-					group, layoutSet, layoutSet.getGroupId(),
-					layoutSet.isPrivateLayout(), parentLayoutIds)) {
-
-				return layoutPersistence.findByG_P_P(
-					layoutSet.getGroupId(), layoutSet.isPrivateLayout(),
-					parentLayoutIds);
-			}
-
 			List<Layout> layouts = layoutPersistence.findByG_P_P(
 				layoutSet.getGroupId(), layoutSet.isPrivateLayout(),
 				parentLayoutIds);
@@ -4586,15 +4566,13 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 			return false;
 		}
 
-		if (Validator.isNull(layout.getPortletLayoutPageTemplateEntryERC()) &&
-			Validator.isNull(layout.getLayoutSetPrototypeLayoutERC())) {
+		if (Validator.isNull(layout.getPortletLayoutPageTemplateEntryERC()) ||
+			Validator.isNotNull(layout.getLayoutSetPrototypeLayoutERC())) {
 
 			return false;
 		}
 
 		boolean workflowEnabled = WorkflowThreadLocal.isEnabled();
-
-		LayoutSet layoutSet = layout.getLayoutSet();
 
 		try {
 			WorkflowThreadLocal.setEnabled(false);
@@ -4602,10 +4580,6 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 			Sites sites = _sitesSnapshot.get();
 
 			sites.mergeLayoutPrototypeLayout(group, layout);
-
-			if (Validator.isNotNull(layout.getLayoutSetPrototypeLayoutERC())) {
-				sites.mergeLayoutSetPrototypeLayouts(group, layoutSet);
-			}
 		}
 		catch (CTTransactionException | PortalException exception) {
 			throw exception;
@@ -4616,46 +4590,6 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 		finally {
 			MergeLayoutPrototypesThreadLocal.setMergeComplete(
 				"getLayout", arguments);
-			WorkflowThreadLocal.setEnabled(workflowEnabled);
-		}
-
-		return true;
-	}
-
-	private boolean _mergeLayouts(
-		Group group, LayoutSet layoutSet, Object... arguments) {
-
-		arguments = ArrayUtil.append(
-			arguments, CTCollectionThreadLocal.getCTCollectionId());
-
-		if ((MergeLayoutPrototypesThreadLocal.isMergeComplete(
-				"getLayouts", arguments) &&
-			 !group.isUser()) ||
-			StartupHelperUtil.isUpgrading()) {
-
-			return false;
-		}
-
-		boolean workflowEnabled = WorkflowThreadLocal.isEnabled();
-
-		try {
-			Sites sites = _sitesSnapshot.get();
-
-			if (sites.isLayoutSetMergeable(group, layoutSet)) {
-				WorkflowThreadLocal.setEnabled(false);
-
-				sites.mergeLayoutSetPrototypeLayouts(group, layoutSet);
-			}
-		}
-		catch (Exception exception) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"Unable to merge layouts for site template", exception);
-			}
-		}
-		finally {
-			MergeLayoutPrototypesThreadLocal.setMergeComplete(
-				"getLayouts", arguments);
 			WorkflowThreadLocal.setEnabled(workflowEnabled);
 		}
 
