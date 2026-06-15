c62a1d35db36e6d4962a0defc5d1309d32c344af
Carlos Correa <carlos.correa@liferay.com>
2026-06-15 12:41:11 +0200

    LPD-87027 Skip the page template merge for layouts linked to a site template
    When a layout is linked to a site template, that is, it has a layout set prototype layout external reference code, the page template merge is no longer applied even when the layout is also linked to a page template. The site template propagation has to be triggered manually through the LayoutSetPrototypeHelper sync flow instead.

--- modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/util/SitesImpl.java
+++ modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/util/SitesImpl.java
@@ -431,32 +431,19 @@ public class SitesImpl implements Sites {
 
 	@Override
 	public void mergeLayoutPrototypeLayout(Layout layout) throws Exception {
-		String layoutSetPrototypeLayoutERC =
-			layout.getLayoutSetPrototypeLayoutERC();
-
-		if (Validator.isNull(layoutSetPrototypeLayoutERC)) {
-			doMergeLayoutPrototypeLayout(layout);
-
+		if (!layout.isPortletLayoutPageTemplateEntryLinkActive()) {
 			return;
 		}
 
-		LayoutSet layoutSet = layout.getLayoutSet();
-
-		long layoutSetPrototypeId = layoutSet.getLayoutSetPrototypeId();
-
-		if (layoutSetPrototypeId > 0) {
-			Group layoutSetPrototypeGroup =
-				_groupLocalService.getLayoutSetPrototypeGroup(
-					layout.getCompanyId(), layoutSetPrototypeId);
-
-			Layout sourcePrototypeLayout =
-				_layoutLocalService.fetchLayoutByExternalReferenceCode(
-					layoutSetPrototypeLayoutERC,
-					layoutSetPrototypeGroup.getGroupId());
-
-			if (sourcePrototypeLayout != null) {
-				doMergeLayoutPrototypeLayout(sourcePrototypeLayout);
+		if (Validator.isNotNull(layout.getLayoutSetPrototypeLayoutERC())) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Merge not performed because the layout is linked to a " +
+						"layout set prototype with external reference code " +
+							layout.getLayoutSetPrototypeLayoutERC());
 			}
+
+			return;
 		}
 
 		doMergeLayoutPrototypeLayout(layout);
