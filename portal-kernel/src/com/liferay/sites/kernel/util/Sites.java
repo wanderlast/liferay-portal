ac586d0c3f253acc03fea267f6110f26e43e10d0
Carlos Correa <carlos.correa@liferay.com>
2026-06-17 10:42:49 +0200

    LPD-87027 SF

--- portal-kernel/src/com/liferay/sites/kernel/util/Sites.java
+++ portal-kernel/src/com/liferay/sites/kernel/util/Sites.java
@@ -95,8 +95,7 @@ public interface Sites {
 	public void mergeLayoutPrototypeLayout(Group group, Layout layout)
 		throws Exception;
 
-	public void mergeLayoutPrototypeLayout(Layout layout)
-		throws Exception;
+	public void mergeLayoutPrototypeLayout(Layout layout) throws Exception;
 
 	public void mergeLayoutSetPrototypeLayouts(Group group, LayoutSet layoutSet)
 		throws Exception;
