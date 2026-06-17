c80ba0b176ab50cf1d9bf80d3e4f4b87207396b8
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-06-17 18:35:50 +0200

    LPD-93455 Test a REST client fragment passes validation on update

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/exportimport/test/FragmentExportImportTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/exportimport/test/FragmentExportImportTest.java
@@ -251,6 +251,8 @@ public class FragmentExportImportTest extends BasePortletExportImportTestCase {
 			_updateFragmentEntry(fragmentEntry, _getRESTClientHTML()));
 		_testExportImportPortletWithValidationWithFragmentEntryContentException(
 			_updateFragmentEntry(fragmentEntry, "${"));
+		_testExportImportPortletWithValidation(
+			_updateFragmentEntry(fragmentEntry, _getRESTClientHTML()));
 	}
 
 	@Test
