218df437fb737ee9a8ad6f8a4d1d9e87b6ff64ba
Carlos Correa <carlos.correa@liferay.com>
2026-06-24 15:21:51 +0200

    LPD-87027 Inject the info necessary into the background tasks in order to build the nodification when all the processes finish

---
+++ modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/exportimport/constants/LayoutSetPrototypeMergeConstants.java
@@ -0,0 +1,21 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.internal.exportimport.constants;
+
+/**
+ * @author Carlos Correa
+ */
+public class LayoutSetPrototypeMergeConstants {
+
+	public static final String LAYOUT_SET_GROUP_IDS = "layoutSetGroupIds";
+
+	public static final String LAYOUT_SET_PROTOTYPE_ID = "layoutSetPrototypeId";
+
+	public static final String PRE_VALIDATION_ERRORS = "preValidationErrors";
+
+	public static final String USER_ID = "userId";
+
+}
