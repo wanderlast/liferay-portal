228fed194c4e733f4b9155099a4b9a90084dbcdb
Jorge García Jiménez <jorge.garcia@liferay.com>
2026-06-25 16:17:04 +0200

    LPD-95987 Test the PwdModifiedFilter logout redirect is not cached

--- portal-impl/test/unit/com/liferay/portal/servlet/filters/password/modified/PasswordModifiedFilterTest.java
+++ portal-impl/test/unit/com/liferay/portal/servlet/filters/password/modified/PasswordModifiedFilterTest.java
@@ -5,14 +5,23 @@
 
 package com.liferay.portal.servlet.filters.password.modified;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.servlet.HttpHeaders;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.ProxyFactory;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.portal.util.PortalImpl;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.Date;
 
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -68,6 +77,103 @@ public class PasswordModifiedFilterTest {
 
 		Mockito.verify(
 			filterChain
+		).doFilter(
+			Mockito.any(), Mockito.any()
+		);
+	}
+
+	@Test
+	public void testProcessFilterWhenPasswordModified() throws Exception {
+		User user = Mockito.mock(User.class);
+
+		Mockito.when(
+			user.getPasswordModifiedDate()
+		).thenReturn(
+			new Date()
+		);
+
+		long userId = RandomTestUtil.randomLong();
+
+		Mockito.when(
+			user.getUserId()
+		).thenReturn(
+			userId
+		);
+
+		PortalUtil portalUtil = new PortalUtil();
+
+		portalUtil.setPortal(
+			new PortalImpl() {
+
+				@Override
+				public String getPathContext() {
+					return StringPool.BLANK;
+				}
+
+				@Override
+				public String getPathMain() {
+					return "/c";
+				}
+
+				@Override
+				public User getUser(HttpServletRequest httpServletRequest) {
+					return user;
+				}
+
+			});
+
+		HttpSession httpSession = Mockito.mock(HttpSession.class);
+
+		Mockito.when(
+			httpSession.getAttribute(WebKeys.USER_ID)
+		).thenReturn(
+			userId
+		);
+
+		PasswordModifiedFilter passwordModifiedFilter =
+			new PasswordModifiedFilter();
+
+		HttpServletResponse httpServletResponse = Mockito.mock(
+			HttpServletResponse.class);
+
+		FilterChain filterChain = Mockito.mock(FilterChain.class);
+
+		passwordModifiedFilter.processFilter(
+			new HttpServletRequestWrapper(
+				ProxyFactory.newDummyInstance(HttpServletRequest.class)) {
+
+				@Override
+				public String getRequestURI() {
+					return "/combo";
+				}
+
+				@Override
+				public HttpSession getSession(boolean create) {
+					return httpSession;
+				}
+
+				@Override
+				public boolean isRequestedSessionIdValid() {
+					return true;
+				}
+
+			},
+			httpServletResponse, filterChain);
+
+		Mockito.verify(
+			httpServletResponse
+		).setHeader(
+			HttpHeaders.CACHE_CONTROL, HttpHeaders.CACHE_CONTROL_NO_CACHE_VALUE
+		);
+
+		Mockito.verify(
+			httpServletResponse
+		).sendRedirect(
+			"/c/portal/logout"
+		);
+
+		Mockito.verify(
+			filterChain, Mockito.never()
 		).doFilter(
 			Mockito.any(), Mockito.any()
 		);
