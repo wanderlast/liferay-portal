94269aa954c96203fb8cc411b3fb70e2c7789ec0
Ben Demetrius <ben.demetrius@liferay.com>
2026-07-06 16:01:56 +0530

    LPD-97230 Resolve string scope keys for the object entry folders page

--- modules/apps/headless/headless-object/headless-object-impl/src/main/java/com/liferay/headless/object/internal/resource/v1_0/ObjectEntryFolderResourceImpl.java
+++ modules/apps/headless/headless-object/headless-object-impl/src/main/java/com/liferay/headless/object/internal/resource/v1_0/ObjectEntryFolderResourceImpl.java
@@ -302,7 +302,7 @@ public class ObjectEntryFolderResourceImpl
 						booleanQuery.getPreBooleanFilter();
 
 					booleanFilter.add(
-						new TermFilter(Field.GROUP_ID, scopeKey),
+						new TermFilter(Field.GROUP_ID, String.valueOf(groupId)),
 						BooleanClauseOccur.MUST);
 				}
 			},
