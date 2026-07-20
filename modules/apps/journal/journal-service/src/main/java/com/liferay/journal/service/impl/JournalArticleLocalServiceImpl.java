a8651d19d9b72db873cfa937662b5a6276bcbd62
Akhash R <akhash.ramprakash@liferay.com>
2026-07-20 12:23:00 +0530

    LPD-97993 Clear the asset entry layoutUuid on display page deletion
    Deleting a display page cleared the layoutUuid on every matching journal article but left the stale value on the corresponding asset entry, causing a StackOverflowError during search suggestions when generating a URL for an asset pointing to a nonexistent layout.

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -1472,6 +1472,11 @@ public class JournalArticleLocalServiceImpl
 			groupId, layoutUuid);
 
 		for (JournalArticle article : articles) {
+			_deleteLayoutAssetEntryReference(
+				article.getPrimaryKey(), layoutUuid);
+			_deleteLayoutAssetEntryReference(
+				article.getResourcePrimKey(), layoutUuid);
+
 			article.setLayoutUuid(StringPool.BLANK);
 
 			journalArticlePersistence.update(article);
@@ -7892,6 +7897,23 @@ public class JournalArticleLocalServiceImpl
 		finally {
 			serviceContext.setIndexingEnabled(indexingEnabled);
 		}
+	}
+
+	private void _deleteLayoutAssetEntryReference(
+		long classPK, String layoutUuid) {
+
+		AssetEntry assetEntry = _assetEntryLocalService.fetchEntry(
+			JournalArticle.class.getName(), classPK);
+
+		if ((assetEntry == null) ||
+			!Objects.equals(layoutUuid, assetEntry.getLayoutUuid())) {
+
+			return;
+		}
+
+		assetEntry.setLayoutUuid(StringPool.BLANK);
+
+		_assetEntryLocalService.updateAssetEntry(assetEntry);
 	}
 
 	private boolean _equals(
