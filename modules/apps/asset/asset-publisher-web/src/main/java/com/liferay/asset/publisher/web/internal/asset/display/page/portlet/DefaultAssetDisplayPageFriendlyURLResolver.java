210e74df6c5d4aebe1ee3556324caab76ecaef28
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-07-22 20:26:37 +0200

    LPD-97821 Declare the layout mode as used

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/asset/display/page/portlet/DefaultAssetDisplayPageFriendlyURLResolver.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/asset/display/page/portlet/DefaultAssetDisplayPageFriendlyURLResolver.java
@@ -250,8 +250,6 @@ public class DefaultAssetDisplayPageFriendlyURLResolver
 		HttpServletRequest httpServletRequest =
 			(HttpServletRequest)requestContext.get("request");
 
-		String layoutMode = ParamUtil.getString(httpServletRequest, "p_l_mode");
-
 		if (Validator.isNull(currentDefaultAssetPublisherPortletId)) {
 			String actualPortletAuthenticationToken = AuthTokenUtil.getToken(
 				httpServletRequest, layout.getPlid(),
@@ -294,6 +292,8 @@ public class DefaultAssetDisplayPageFriendlyURLResolver
 		actualParams.put(
 			namespace + "assetEntryId",
 			new String[] {String.valueOf(assetEntry.getEntryId())});
+
+		String layoutMode = ParamUtil.getString(httpServletRequest, "p_l_mode");
 
 		if (Objects.equals(layoutMode, Constants.PREVIEW)) {
 			_markWorkflowAssetPreview(
