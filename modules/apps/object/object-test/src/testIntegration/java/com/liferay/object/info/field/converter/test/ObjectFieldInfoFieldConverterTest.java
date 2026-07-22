ec031c40f4b0e1ecf62016676e540017e79546c6
Lianne Louie <lianne.louie@liferay.com>
2026-07-21 23:48:53 -0700

    LPD-97608 add object tests

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/info/field/converter/test/ObjectFieldInfoFieldConverterTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/info/field/converter/test/ObjectFieldInfoFieldConverterTest.java
@@ -36,6 +36,7 @@ import com.liferay.object.service.ObjectEntryLocalService;
 import com.liferay.object.service.ObjectFieldLocalService;
 import com.liferay.object.service.ObjectFieldSettingLocalService;
 import com.liferay.object.service.ObjectRelationshipLocalService;
+import com.liferay.object.system.SystemObjectDefinitionManagerRegistry;
 import com.liferay.object.test.util.ObjectDefinitionTestUtil;
 import com.liferay.object.test.util.ObjectRelationshipTestUtil;
 import com.liferay.petra.string.StringPool;
@@ -127,7 +128,8 @@ public class ObjectFieldInfoFieldConverterTest {
 				null, null, null, _objectDefinitionLocalService,
 				_objectFieldLocalService, null, _objectRelationshipLocalService,
 				_objectScopeProviderRegistry, null, null, _portal,
-				_restContextPathResolverRegistry, null);
+				_restContextPathResolverRegistry,
+				_systemObjectDefinitionManagerRegistry, null);
 
 		InfoField.FinalStep finalStep = InfoField.builder(
 		).infoFieldType(
@@ -198,12 +200,51 @@ public class ObjectFieldInfoFieldConverterTest {
 	}
 
 	@Test
+	public void testAddRelationshipInfoFieldAttributesWithOrganization()
+		throws Exception {
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.fetchSystemObjectDefinition(
+				TestPropsValues.getCompanyId(), "Organization");
+
+		ObjectRelationship objectRelationship =
+			ObjectRelationshipTestUtil.addObjectRelationship(
+				_objectRelationshipLocalService, objectDefinition,
+				_objectDefinition);
+
+		ObjectFieldInfoFieldConverter objectFieldInfoFieldConverter =
+			new ObjectFieldInfoFieldConverter(
+				null, null, null, _objectDefinitionLocalService,
+				_objectFieldLocalService, null, _objectRelationshipLocalService,
+				_objectScopeProviderRegistry, null, null, _portal,
+				_restContextPathResolverRegistry,
+				_systemObjectDefinitionManagerRegistry, null);
+
+		InfoField.FinalStep finalStep = InfoField.builder(
+		).infoFieldType(
+			RelationshipInfoFieldType.INSTANCE
+		).namespace(
+			RandomTestUtil.randomString()
+		).name(
+			RandomTestUtil.randomString()
+		);
+
+		Group group = GroupTestUtil.addGroup();
+
+		String url = _getRelationshipURL(
+			finalStep, objectFieldInfoFieldConverter, objectRelationship,
+			_getServiceContext(group.getGroupId(), null));
+
+		Assert.assertTrue(url, url.endsWith("?flatten=true&fields=id,name"));
+	}
+
+	@Test
 	public void testGetInfoField() throws Exception {
 		ObjectFieldInfoFieldConverter objectFieldInfoFieldConverter =
 			new ObjectFieldInfoFieldConverter(
 				_ddmExpressionFactory, null, null, null, null,
 				_objectFieldSettingLocalService, null, null, null, null, null,
-				null, null);
+				null, null, null);
 
 		InfoField<?> infoField = objectFieldInfoFieldConverter.getInfoField(
 			true, ObjectField.class.getSimpleName(), _objectField);
@@ -275,7 +316,7 @@ public class ObjectFieldInfoFieldConverterTest {
 			new ObjectFieldInfoFieldConverter(
 				_ddmExpressionFactory, null, null, null, null,
 				_objectFieldSettingLocalService, null, null, null, null, null,
-				null, null);
+				null, null, null);
 
 		InfoField<PhoneNumberInfoFieldType> infoField =
 			(InfoField<PhoneNumberInfoFieldType>)
@@ -407,5 +448,9 @@ public class ObjectFieldInfoFieldConverterTest {
 
 	@Inject
 	private RESTContextPathResolverRegistry _restContextPathResolverRegistry;
+
+	@Inject
+	private SystemObjectDefinitionManagerRegistry
+		_systemObjectDefinitionManagerRegistry;
 
 }
