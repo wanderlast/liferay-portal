680ae0e0dfe7159bb7d26cae00d99abfc19e550d
Brian Wulbern <brian.wulbern@liferay.com>
2026-07-29 16:40:29 -0500

    LPD-88540 site-cms ObjectEntryLocalServiceTest: pass a non-null className to addSystemObjectDefinition (backport master c82fa75, LPD-81550)

--- modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/service/test/ObjectEntryLocalServiceTest.java
@@ -113,8 +113,9 @@ public class ObjectEntryLocalServiceTest {
 		ObjectDefinition objectDefinition =
 			_objectDefinitionLocalService.addSystemObjectDefinition(
 				null, TestPropsValues.getUserId(),
-				objectFolder.getObjectFolderId(), null, null, false, true,
-				false, true, false, false, false, false, false, null,
+				objectFolder.getObjectFolderId(),
+				ObjectDefinitionUtil.generateRandomClassName(), null, false,
+				true, false, true, false, false, false, false, false, null,
 				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
 				true, "Test", null, null, null, null,
 				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
