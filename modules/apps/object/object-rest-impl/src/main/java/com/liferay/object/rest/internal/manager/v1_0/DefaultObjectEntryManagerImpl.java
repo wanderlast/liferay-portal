42780836200b7b362e853e7efebef87ec322d11f
joshitagaki <joshua.itagaki@liferay.com>
2026-08-03 09:14:04 -0700

    LPD-100598 Revert "LPD-75874 Use the request preferred locale for object entry search"
    This reverts commit 4e7d0d7ba28637e20b25fcf64aa1249fbc4b75fc.

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -880,7 +880,6 @@ public class DefaultObjectEntryManagerImpl
 
 				searchContext.setCompanyId(companyId);
 				searchContext.setGroupIds(new long[] {groupId});
-				searchContext.setLocale(dtoConverterContext.getLocale());
 
 				SearchRequestBuilder searchRequestBuilder =
 					_searchRequestBuilderFactory.builder(searchContext);
