9f2bd955d4bb97c7c4e1e69bb78a1dfd86f83f61
Selena Aungst <selena.aungst@liferay.com>
2026-08-11 14:27:27 -0700

    LPD-89174 portal-search-tuning-synonyms-web-test: drop the enableCategorization argument absent on release-2026.q1.
    https://liferay.atlassian.net/browse/LPD-89174

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web-test/src/testIntegration/java/com/liferay/portal/search/tuning/synonyms/web/internal/index/test/ObjectEntrySynonymSearchTest.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web-test/src/testIntegration/java/com/liferay/portal/search/tuning/synonyms/web/internal/index/test/ObjectEntrySynonymSearchTest.java
@@ -120,7 +120,7 @@ public class ObjectEntrySynonymSearchTest {
 	private ObjectDefinition _addObjectDefinition(User user) throws Exception {
 		ObjectDefinition objectDefinition =
 			_objectDefinitionLocalService.addCustomObjectDefinition(
-				null, user.getUserId(), 0, null, true, false, true, false, true,
+				null, user.getUserId(), 0, null, false, true, false, true,
 				false, false, false, false, null,
 				LocalizedMapUtil.getLocalizedMap(
 					ObjectDefinitionTestUtil.getRandomName()),
